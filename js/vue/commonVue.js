79898ef3840cb392030508046ec958ea9e9ca384
d0rich <d.orich@yandex.ru>
2020-04-21 21:15:38 +0700

    Начал немного работать с Vue js

---
+++ js/vue/commonVue.js
@@ -0,0 +1,18 @@
+new Vue ({
+  el: '#NavPanel',
+  data: {
+    Social:[
+      {imglink:'img/Social/vk.svg', link:'https://vk.com/d0rich', nick:'@d0rich' },
+      {imglink:'img/Social/vk.svg', link:'https://vk.com/@d0rich_prod.', nick:'@d0rich' },
+      {imglink:'img/Social/instagram.svg', link:'https://vk.com/d0rich', nick:'@d0rich' },
+      {imglink:'img/Social/youtube.svg', link:'https://vk.com/d0rich', nick:'@d0rich' }
+    ],
+    Sections:[
+      {link:'index.html', name:'Главная'},
+      {link:'index.html', name:'Все проекты'},
+      {link:'prices.html', name:'Расценки'},
+      {link:'index.html', name:'Пункт 4'},
+      {link:'index.html', name:'Пункт 5'}
+    ]
+  }
+})
---
+++ js/vue/mainVue.js
@@ -0,0 +1,18 @@
+
+new Vue ({
+  el: '#MainContent',
+  data: {
+    Text1:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad',
+    PresLink: 'https://player.vimeo.com/video/388640735',
+    Screenshots :[
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'},
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'},
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'},
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'},
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'},
+    {description:'Lorem ipsum dolor sit amet, consectetuer adipisc', timecode:'X:XX - X:XX', screenlink:'img/Main/Screenshot.png'}],
+
+  },
+  components: {
+  }
+})
