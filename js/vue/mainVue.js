62ac16bb50d2fdf985e2072f4afcfe4a5f655fdd
d0rich <d.orich@yandex.ru>
2020-04-21 22:49:42 +0700

    Добавил мои проекты

--- js/vue/mainVue.js
+++ js/vue/mainVue.js
@@ -1,4 +1,3 @@
-
 new Vue ({
   el: '#MainContent',
   data: {
---
+++ js/vue/projectsVue.js
@@ -0,0 +1,9 @@
+new Vue ({
+  el: '#ProjectsContent',
+  data: {
+    Prevues:[
+      {MainImg:'img/Projects/Sobitie/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"', link:'projects-id1.html', screens:['img/Projects/Sobitie/1.png','img/Projects/Sobitie/2.png']}
+    ]
+
+  }
+})
