edf85bec8cb0502c49bf1ac3ee77bb5807d1f000
d0rich <d.orich@yandex.ru>
2020-04-22 13:15:41 +0700

    Добавил немного переходов

--- js/vue/projectsVue.js
+++ js/vue/projectsVue.js
@@ -1,9 +1,13 @@
-new Vue ({
+var Projects = new Vue ({
   el: '#ProjectsContent',
   data: {
+    ProjectsShow: true,
     Prevues:[
       {MainImg:'img/Projects/Sobitie/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"', link:'projects-id1.html', screens:['img/Projects/Sobitie/1.png','img/Projects/Sobitie/2.png']}
     ]
 
+  },
+  methods:{
+    
   }
 })
