4493f7e011b895d6175ce8fe18be1ed1dfa46547
d0rich <d.orich@yandex.ru>
2020-04-22 14:47:13 +0700

    Реализовал выбор проекта

--- js/vue/projectsVue.js
+++ js/vue/projectsVue.js
@@ -1,13 +1,41 @@
+var search = function(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].id === nameKey) {
+            return myArray[i];
+        }
+    }
+}
 var Projects = new Vue ({
   el: '#ProjectsContent',
   data: {
     ProjectsShow: true,
+    NowVideoLink: 'https://player.vimeo.com/video/388640735',
+    NowProjectName: 'Тизер для "События"',
+    NowHashtags: '#Video #MotionDesign #Compositing #Teaser',
+    NowDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad',
+    NowScreens: [],
     Prevues:[
-      {MainImg:'img/Projects/Sobitie/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"', link:'projects-id1.html', screens:['img/Projects/Sobitie/1.png','img/Projects/Sobitie/2.png']}
+      {id:'001', MainImg:'img/Projects/Sobitie_2019/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"',
+        link:'projects-id1.html', mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
+        videolink: 'https://player.vimeo.com/video/388640735',
+        description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad'},
+      {id:'002', MainImg:'img/Projects/Proriv_2020/Main.png', HashTags:'#Video #MotionDesign #Compositing #Teaser', name:'Тизер для ССервО "Прорыв"',
+        link:'projects-id1.html', mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
+        videolink: 'https://player.vimeo.com/video/388779111',
+        description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad'}
     ]
 
   },
   methods:{
-    
+      NowPage(event){
+      id=event.target.id;
+      PageData=search(id, this.Prevues);
+      this.ProjectsShow = false;
+      this.NowVideoLink = PageData.videolink;
+      this.NowProjectName = PageData.name;
+      this.NowHashtags = PageData.hashtags;
+      this.NowDescription = PageData.description;
+      this.NowScreens = [];
+    }
   }
 })
