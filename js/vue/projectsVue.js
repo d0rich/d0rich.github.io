90e83969b02597959aa31dad69d8511696e290e7
d0rich <d.orich@yandex.ru>
2020-04-23 15:52:43 +0700

    BigProjectPattern

--- js/vue/projectsVue.js
+++ js/vue/projectsVue.js
@@ -9,38 +9,65 @@ var search = function(nameKey, myArray){
 var Projects = new Vue ({
   el: '#ProjectsContent',
   data: {
-    ProjectsShow: true,
+    videoShow: false,
+    bigProjectShow: false,
+    PageData: null,
     NowVideoLink: 'https://player.vimeo.com/video/388640735',
     NowProjectName: 'Тизер для "События"',
     NowHashtags: '#Video #MotionDesign #Compositing #Teaser',
     NowDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad',
     NowScreens: [],
     Prevues:[
-      {id:'001', MainImg:'img/Projects/Sobitie_2019/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"',
-        link:'projects-id1.html', mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
+      {id:'001', Big:false, MainImg:'img/Projects/Sobitie_2019/Main.png', HashTags:'#Video #MotionDesign #Teaser', name:'Тизер для "События"',
+        mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
         videolink: 'https://player.vimeo.com/video/388640735',
         description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad'},
-      {id:'002', MainImg:'img/Projects/Proriv_2020/Main.png', HashTags:'#Video #MotionDesign #Compositing #Teaser', name:'Тизер для ССервО "Прорыв"',
-        link:'projects-id1.html', mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
+      {id:'002', Big:false, MainImg:'img/Projects/Proriv_2020/Main.png', HashTags:'#Video #MotionDesign #Compositing #Teaser', name:'Тизер для ССервО "Прорыв"',
+        mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
         videolink: 'https://player.vimeo.com/video/388779111',
-        description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad'}
+        description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad'},
+      {id:'003', Big:true, MainImg:'img/Projects/Proriv_2020/Main.png', HashTags:'#GameDev', name:'Игра Splash: Glory and victims',
+        mainscreens:['img/Projects/Sobitie_2019/1.png','img/Projects/Sobitie_2019/2.png'],
+        FirstImg:[], videolink: ['https://player.vimeo.com/video/388779111'],
+        description:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad',
+        Tiles: [
+          {video:[], mainimg:['img/Projects/Sobitie_2019/1.png'] ,
+           subimg:['img/Projects/Match3_2020/BlueBot.svg', 'img/Projects/Match3_2020/RedBot.svg', 'img/Projects/Match3_2020/GreenBot.svg', 'img/Projects/Match3_2020/OrangeBot.svg'],
+           subdescription:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit'},
+          {video:[], mainimg:['img/Projects/Sobitie_2019/1.png'] ,
+           subimg:['img/Projects/Match3_2020/BlueBot.svg', 'img/Projects/Match3_2020/RedBot.svg', 'img/Projects/Match3_2020/GreenBot.svg', 'img/Projects/Match3_2020/OrangeBot.svg'],
+           subdescription:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit'}
+        ]
+      },
     ]
 
+  },
+  computed:{
+    ProjectsListShow: function(){
+      if (this.videoShow || this.bigProjectShow)
+      return false
+      else return true
+    }
   },
   methods:{
       NowPage(event){
         id=event.target.id;
-        PageData=search(id, this.Prevues);
-        this.ProjectsShow = false;
-        this.NowVideoLink = PageData.videolink;
-        this.NowProjectName = PageData.name;
-        this.NowHashtags = PageData.HashTags;
-        this.NowDescription = PageData.description;
-        this.NowScreens = [];
+        this.PageData=search(id, this.Prevues);
+        if (this.PageData.Big)
+        this.NowBig();
+        else this.NowVideo();
         document.getElementById('Heading').classList.add("ActiveShow");
       },
+      NowVideo(){
+        this.videoShow = true;
+      },
+      NowBig(){
+        this.bigProjectShow = true;
+      },
       NowProjects(){
-        this.ProjectsShow = true;
+        this.videoShow = false;
+        this.bigProjectShow = false;
+        this.PageData = null;
         document.getElementById('Heading').classList.remove("ActiveShow");
       }
   }
