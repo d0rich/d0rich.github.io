80ca7a22943155f1d35def5141c6a520e510b42b
d0rich <d.orich@yandex.ru>
2021-01-21 20:34:05 +0700

    home enter image 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <Terminal reveal enter-word="hello world">
-      <h1 class="align-self-start cursor-on-hover">{{data.h1.text}}<span class="cursor">_</span></h1>
-      <p class="align-self-start cursor-on-hover">
-        {{data.p1.text}}<span class="cursor">_</span>
-      </p>
-      <p class="align-self-start cursor-on-hover">
-        {{data.p2.t1.text}}
-        <router-link class="link" :to="{name: 'Portfolio'}">{{data.p2.t2.text}}</router-link>{{data.p2.t3.text}}
-        <router-link class="link" :to="{name: 'About'}">{{data.p2.t4.text}}</router-link>{{data.p2.t5.text}}
-        <router-link class="link" :to="{name: 'Blog'}">{{data.p2.t6.text}}</router-link>{{data.p2.t7.text}}<span class="cursor">_</span>
-      </p>
-    </Terminal>
+    <HomeEnterImage />
+    <v-lazy :options="{threshold: 1}">
+      <Terminal reveal enter-word="hello world">
+        <h1 class="align-self-start cursor-on-hover">{{data.h1.text}}<span class="cursor">_</span></h1>
+        <p class="align-self-start cursor-on-hover">
+          {{data.p1.text}}<span class="cursor">_</span>
+        </p>
+        <p class="align-self-start cursor-on-hover">
+          {{data.p2.t1.text}}
+          <router-link class="link" :to="{name: 'Portfolio'}">{{data.p2.t2.text}}</router-link>{{data.p2.t3.text}}
+          <router-link class="link" :to="{name: 'About'}">{{data.p2.t4.text}}</router-link>{{data.p2.t5.text}}
+          <router-link class="link" :to="{name: 'Blog'}">{{data.p2.t6.text}}</router-link>{{data.p2.t7.text}}<span class="cursor">_</span>
+        </p>
+      </Terminal>
+    </v-lazy>
+
 
     <div style="width: 100%; height: 300vh"></div>
   </div>
@@ -19,12 +23,13 @@
 
 <script>
 import homepage from '@/data/home'
-import Terminal from "@/components/Terminal";
+import Terminal from "@/components/Terminal"
+import HomeEnterImage from "@/components/home/EnterBlock1";
 
 export default {
   name: 'Home',
   components: {
-    Terminal
+    Terminal, HomeEnterImage
   },
   data(){
     return{
