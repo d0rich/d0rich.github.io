f2cc5bc4656f112ac839c36b357734693c498dbe
d0rich <d.orich@yandex.ru>
2021-02-01 21:38:09 +0700

    home page version 1

--- src/data/home/index.js
+++ src/data/home/index.js
@@ -1,37 +1,57 @@
 import {JSField, JSObject, Text} from "@/classes";
 import my_photo from '@/assets/home/dorich.jpg'
 
-
-import pic1 from './pic1.svg'
 export default {
-    h1: new Text('Привет мир!', 'Hello world!'),
-    pic1: pic1,
-    p1: {
-        t1: new Text('Привет, меня зовут Николай Дорофеев.', 'Hello, my name is Nikolay Dorofeev.'),
-        t2: new Text('Я ', 'I am '),
-        t3: new Text('JavaScript разработчик', 'JavaScript developer'),
-        t4: new Text('И это мой личный сайт.', 'And this is my own site.')
+    block1:{
+        p1: {
+            t1: new Text('Привет, меня зовут Николай Дорофеев.', 'Hello, my name is Nikolay Dorofeev.'),
+            t2: new Text('Я ', 'I am '),
+            t3: new Text('JavaScript разработчик', 'JavaScript developer'),
+            t4: new Text('И это мой личный сайт.', 'And this is my own site.')
+        },
     },
-    p2: {
-        t1: new Text('На данном сайте вы можете ознакомиться с моим '),
-        t2: new Text('портфолио'),
-        t3: new Text(', узнать побольше информации '),
-        t4: new Text('обо мне'),
-        t5: new Text(' и моих умениях, почитать мой '),
-        t6: new Text('блог'),
-        t7: new Text(', может увидите что-то интересное :).')
-    },
-    my_photo: my_photo,
-    obj1: new JSObject([
-        new JSField( { key: new Text('Имя', 'Name'), value: new Text('Николай', 'Nikolay') } ),
-        new JSField( { key: new Text('Фамилия', 'Surname'), value: new Text('Дорофеев', 'Dorofeev') } ),
-        new JSField( { key: new Text('Возраст', 'Age'), value: new Text('20', '20') } ),
-        new JSField( { key: new Text('Статус', 'Status'), value: new Text('Студент', 'Student') } )
-    ], new Text('Личность', 'Person')),
-    obj2: new JSObject([
-        new JSField( { key: new Text('Имя', 'Name'), value: new Text('Николай', 'Nikolay') } ),
-        new JSField( { key: new Text('Фамилия', 'Surname'), value: new Text('Дорофеев', 'Dorofeev') } ),
-        new JSField( { key: new Text('Возраст', 'Age'), value: new Text('20', '20') } ),
-        new JSField( { key: new Text('Статус', 'Status'), value: new Text('Студент', 'Student') } )
-    ], new Text('Личность', 'Person'))
+    block2: {
+        terminal: {
+            h1: new Text('Привет мир!', 'Hello world!'),
+            t1: new Text('На данном сайте вы можете ознакомиться с моим ', 'Here you can see my '),
+            t2: new Text('портфолио', 'portfolio'),
+            t3: new Text(', узнать побольше информации ', ', learn more information '),
+            t4: new Text('обо мне', 'about me'),
+            t5: new Text(' и моих умениях, почитать мой ', ' and my skills, read my '),
+            t6: new Text('блог', 'blog'),
+            t7: new Text(', может увидите что-то интересное :).', ', may be you will find something interesting :).')
+        },
+        my_photo: my_photo,
+        h1: new Text('Коротко обо мне', 'Little about me') ,
+        p1: new Text('Молодой, талантливый, энергичный. \n' +
+            'Меня зовут Николай Дорофеев, я JavaScript разработчик, сейчас обучаюсь на 3-ем курсе ТПУ ' +
+            'по направлению "Информационные системы и технологии в бизнесе и промышленности".\n' +
+            'Полученных знаний и собственного опыта работы с вёрсткой сайтов, базами данных, серверами ' +
+            'уже вполне достаточно, чтобы создавать специально для ВАС ' +
+            'так называемые Single Page Application (SPA) - сайты, которые не требуют загрузки отдельных страниц.\n' +
+            'Этот подход позволяет получить новый опыт взаимодействия с продуктом без лишних задержек.',
+            'Young, talented, energetic. \n' +
+            'My name is Nikolay Dorofeev, I am a JavaScript developer, now I am a 3rd year student at TPU ' +
+            'in the direction of "Information systems and technologies in business and industry".\n' +
+            'The acquired knowledge and personal experience of working with site layout, databases, and servers ' +
+            'is already quite enough to create so-called Single Page Application (SPA) specially for YOU ' +
+            '- sites that do not require loading separate pages.\n' +
+            'This approach allows you to get a new experience with the product without unnecessary delays.'),
+        objects: [
+            new JSObject([
+                new JSField( { key: new Text('Имя', 'Name'), value: new Text('Николай', 'Nikolay') } ),
+                new JSField( { key: new Text('Фамилия', 'Surname'), value: new Text('Дорофеев', 'Dorofeev') } ),
+                new JSField( { key: new Text('Возраст', 'Age'), value: new Text('20', '20') } ),
+                new JSField( { key: new Text('Статус', 'Status'), value: new Text('Студент', 'Student') } )
+            ], new Text('Личность', 'Person')),
+            new JSObject([
+                new JSField( { key: new Text('Никнейм', 'Nickname'), value: new Text('d0rich', 'd0rich') } ),
+                new JSField( { key: new Text('Фронтенд', 'Frontend'), value: new Text('Vue.js', 'Vue.js') } ),
+                new JSField( { key: new Text('Бэкенд', 'Backend'), value: new Text('Node.js', 'Node.js') } ),
+                new JSField( { key: new Text('База данных', 'Database'), value: new Text('PostgreSQL, MongoDB', 'PostgreSQL, MongoDB') } )
+            ], new Text('Разработчик', 'Developer'))
+        ]
+
+    }
+
 }
