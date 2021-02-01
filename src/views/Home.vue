16f29f47438846a72d76c522b191d500ab96099c
d0rich <d.orich@yandex.ru>
2021-02-01 21:38:09 +0700

    home page version 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,47 @@
 <template>
   <div>
     <EnterBlock2 class="text-center">
-      <span style="font-size: 1.7rem;" class="mx-3">{{data.p1.t1.text}}</span>
-      <span style="font-size: 1.7rem;" class="ma-3">{{data.p1.t2.text}}
-        <span style="font-size: 2.3rem; font-weight: bold">{{data.p1.t3.text}}<span class="cursor">_</span></span>
+      <span style="font-size: 1.7rem;" class="mx-3">{{data.block1.p1.t1.text}}</span>
+      <span style="font-size: 1.7rem;" class="ma-3">{{data.block1.p1.t2.text}}
+        <span style="font-size: 2.3rem; font-weight: bold">{{data.block1.p1.t3.text}}<span class="cursor">_</span></span>
       </span>
-      <span style="font-size: 1.7rem;" class="mx-3">{{data.p1.t4.text}}</span>
+      <span style="font-size: 1.7rem;" class="mx-3">{{data.block1.p1.t4.text}}</span>
     </EnterBlock2>
-    <v-lazy :options="{threshold: .7}">
-      <div class="block2 mt-16">
-        <v-lazy :options="{threshold: .7}">
-          <Terminal reveal enter-word="hello world">
-            <h1 class="align-self-start cursor-on-hover">{{data.h1.text}}<span class="cursor">_</span></h1>
-            <p class="align-self-start cursor-on-hover">
-              {{data.p2.t1.text}}
-              <router-link class="link" :to="{name: 'Portfolio'}">{{data.p2.t2.text}}</router-link>{{data.p2.t3.text}}
-              <router-link class="link" :to="{name: 'About'}">{{data.p2.t4.text}}</router-link>{{data.p2.t5.text}}
-              <router-link class="link" :to="{name: 'Blog'}">{{data.p2.t6.text}}</router-link>{{data.p2.t7.text}}<span class="cursor">_</span>
-            </p>
-          </Terminal>
-        </v-lazy>
+    <div class="block2 mt-16">
+      <v-lazy :options="{threshold: 0.7}" min-width="300" min-height="200px" transition="glitch-transition">
+        <Terminal reveal enter-word="hello world" @revealed="show.block2info = true">
+          <h1 class="align-self-start cursor-on-hover">{{data.block2.terminal.h1.text}}<span class="cursor">_</span></h1>
+          <p class="align-self-start cursor-on-hover">
+            {{data.block2.terminal.t1.text}}
+            <router-link class="link" :to="{name: 'Portfolio'}">{{data.block2.terminal.t2.text}}</router-link>{{data.block2.terminal.t3.text}}
+            <router-link class="link" :to="{name: 'About'}">{{data.block2.terminal.t4.text}}</router-link>{{data.block2.terminal.t5.text}}
+            <router-link class="link" :to="{name: 'Blog'}">{{data.block2.terminal.t6.text}}</router-link>{{data.block2.terminal.t7.text}}<span class="cursor">_</span>
+          </p>
+        </Terminal>
+      </v-lazy>
 
+      <v-lazy v-model="show.block2info" min-width="300" min-height="500px" :options="{threshold: 1}" transition="glitch-transition">
         <div class="block2__info">
           <div class="info-row">
-            <img :src="data.my_photo" alt="d0rich" class="border-light--accent my-photo" />
+            <img :src="data.block2.my_photo" alt="d0rich" class="border-light--accent my-photo" />
             <div class="block2__info__text">
-              <h1>Заголовок</h1>
-              <p>С другой стороны укрепление и развитие структуры обеспечивает участие в формировании систем массового участия. Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности обеспечивает широкому кругу (специалистов) участие в формировании новых предложений.</p>
+              <h1>{{data.block2.h1.text}}</h1>
+              <p v-for="(text, index) in data.block2.p1.text.split('\n')" :key="index" >{{text}}</p>
+              <v-btn :to="{name: 'About'}" color="primary">{{btns.myInfo.text}}</v-btn>
             </div>
           </div>
 
           <div class="js-objects">
-            <JSObjectWindow class="mb-3 mx-3" :object="data.obj1" />
-            <JSObjectWindow class="mb-3 mx-3" :object="data.obj2" />
-            <JSObjectWindow class="mb-3 mx-3" :object="data.obj1" />
+            <JSObjectWindow class="mb-3 mx-3"
+                            v-for="(object, index) in data.block2.objects"
+                            :key="index"
+                            :object="object" />
           </div>
         </div>
-      </div>
+      </v-lazy>
 
+    </div>
 
-    </v-lazy>
 
   </div>
 </template>
@@ -49,6 +51,7 @@ import homepage from '@/data/home'
 import Terminal from "@/components/Terminal"
 import JSObjectWindow from "@/components/JSObjectWindow";
 import EnterBlock2 from "@/components/home/EnterBlock2";
+import {Text} from "@/classes";
 
 export default {
   name: 'Home',
@@ -57,12 +60,20 @@ export default {
   },
   data(){
     return{
-      data: homepage
+      data: homepage,
+      btns: {
+        myInfo: new Text('Узнать больше', 'More info')
+      },
+      show: {
+        block2info: false
+      }
     }
   },
-  metaInfo: {
-    title: 'Dorich',
-    titleTemplate: null
+  metaInfo() {
+    return {
+      title: new Text('Главная', 'Home').text
+    }
+
   }
 }
 </script>
@@ -75,17 +86,13 @@ export default {
   align-items: center;
 }
 .block2__info{
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  justify-content: center;
   margin: 2rem 0;
-
+  transition: opacity .5s ease;
   .info-row{
     display: flex;
     flex-direction: row;
     align-items: flex-start;
-    max-width: 700px;
+    margin-bottom: 2rem;
   }
 
   .my-photo{
@@ -97,17 +104,8 @@ export default {
   }
   .js-objects{
     display: flex;
-    flex-direction: column;
-  }
-}
-@media screen and (max-width: 1100px){
-  .block2__info{
-    .js-objects{
-      justify-content: center;
-      flex-direction: row;
-      flex-wrap: wrap;
-      max-width: 700px;
-    }
+    flex-wrap: wrap;
+    justify-content: center;
   }
 }
 @media screen and (max-width: 550px){
