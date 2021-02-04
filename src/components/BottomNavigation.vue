8140d15b0161102d762ab50044846b6bb45af94c
d0rich <d.orich@yandex.ru>
2021-02-04 10:12:15 +0700

    navigation fix

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -2,32 +2,32 @@
   <div>
     <LangSwitcher />
     <v-bottom-navigation class="bottom-nav" color="primary lighten-1">
-      <v-btn :to="navigation.blog.route"  >
+      <v-btn active-class="nav-btn" :to="navigation.blog.route"  >
       <span>
-        {{navigation.blog.text.getText(lang)}}<span style="opacity: 0" :class="{cursor: routeNow === navigation.blog.route.name}">_</span>
+        {{navigation.blog.text.getText(lang)}}<span style="opacity: 0" class="cursor">_</span>
       </span>
         <v-icon>mdi-message</v-icon>
       </v-btn>
 
-      <v-btn :to="navigation.about.route"  >
+      <v-btn active-class="nav-btn" :to="navigation.about.route"  >
       <span>
-        {{navigation.about.text.getText(lang)}}<span style="opacity: 0" :class="{cursor: routeNow === navigation.about.route.name}">_</span>
+        {{navigation.about.text.getText(lang)}}<span style="opacity: 0" class="cursor">_</span>
       </span>
         <v-icon>mdi-account</v-icon>
       </v-btn>
 
 
 
-      <v-btn exact :to="navigation.home.route"  >
+      <v-btn active-class="nav-btn" exact :to="navigation.home.route"  >
       <span>
-        {{navigation.home.text.getText(lang)}}<span style="opacity: 0" :class="{cursor: routeNow === navigation.home.route.name}">_</span>
+        {{navigation.home.text.getText(lang)}}<span style="opacity: 0" class="cursor">_</span>
       </span>
         <v-icon>mdi-console-line</v-icon>
       </v-btn>
 
-      <v-btn :to="navigation.portfolio.route"  >
+      <v-btn active-class="nav-btn" :to="navigation.portfolio.route"  >
       <span>
-        {{navigation.portfolio.text.getText(lang)}}<span style="opacity: 0" :class="{cursor: routeNow === navigation.portfolio.route.name}">_</span>
+        {{navigation.portfolio.text.getText(lang)}}<span style="opacity: 0" class="cursor">_</span>
       </span>
         <v-icon>mdi-file-presentation-box</v-icon>
       </v-btn>
