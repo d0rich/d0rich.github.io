6fc5934fba9d12bc34f86222c1e6c0579a273971
d0rich <d.orich@yandex.ru>
2021-02-08 09:17:37 +0700

    Revert "version 2.0"
    This reverts commit af93afe016004b8224a015ef6e3df75f9309860c.

---
+++ precache-manifest.8a6fb37dafefda1b14f956b9a6a56453.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e15b2a82c85a95b4a90",
+    "url": "/css/app.4a2fae45.css"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/fonts/Comfortaa-VariableFont_wght.f40889ca.ttf"
+  },
+  {
+    "revision": "d1046e2c6a89073a0d7c90b901b332a7",
+    "url": "/img/LoaderMan1.d1046e2c.svg"
+  },
+  {
+    "revision": "6327365f1b786a0d0c237cae3ce969f0",
+    "url": "/img/LoaderMan2.6327365f.svg"
+  },
+  {
+    "revision": "a6081f7a46070f5718024ddc3c415fd7",
+    "url": "/img/LoaderMan3.a6081f7a.svg"
+  },
+  {
+    "revision": "755834a29a29203e9ce084b7622d30d8",
+    "url": "/img/LogoD.755834a2.svg"
+  },
+  {
+    "revision": "1bd78dbc16caca2cab94a05e2b373924",
+    "url": "/img/error404.1bd78dbc.svg"
+  },
+  {
+    "revision": "56cb93da26b9dde3e86a3eeb7fa07bf6",
+    "url": "/img/telegram.56cb93da.svg"
+  },
+  {
+    "revision": "b03024c4d635b0a304e483e1d9fa34d8",
+    "url": "/img/vk.b03024c4.svg"
+  },
+  {
+    "revision": "a6372485318244c18a497ae9a9b62467",
+    "url": "/img/youtube.a6372485.svg"
+  },
+  {
+    "revision": "f0a34a1b5c0f40e16c3965fa519a79a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e15b2a82c85a95b4a90",
+    "url": "/js/app.8bb72e3a.js"
+  },
+  {
+    "revision": "04dbea6ff237a05d332b",
+    "url": "/js/chunk-vendors.1e7cb4e0.js"
+  },
+  {
+    "revision": "09b9d5182fb8a0bfc0727695b4305aab",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
