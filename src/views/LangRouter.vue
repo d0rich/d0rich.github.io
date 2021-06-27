fa9b6a5107e9860bd728fc5b0ec7276babd052f6
Nikolay Dorofeev <d.orich@yandex.com>
2021-06-27 15:20:07 +0700

    Page view events refactoring

--- src/views/LangRouter.vue
+++ src/views/LangRouter.vue
@@ -5,6 +5,7 @@
 <script>
 import {languages} from "@/data/languages";
 import {Text} from "@/classes/text";
+import {routesNames} from "@/data/constants";
 import {mapMutations} from 'vuex'
 
 export default {
@@ -40,26 +41,67 @@ name: "LangRouter",
   },
   beforeRouteUpdate(to, from, next){
     try {
-      this.$analytics.logEvent('page_view', {
-        project_id: to.params.stringId || undefined,
-        resume_id: to.params.resumeId || undefined,
-        query: JSON.stringify(this.$route.query),
+      const pageData = {
         lang: to.params.lang || undefined,
         page_location: document.location,
         page_path: document.location.origin + '/#' + to.path,
         page_referrer: from ? document.location.origin + '/#' + from.fullPath : document.referrer,
         page_referrer_name: from.name || 'external_source',
         ignore_referrer: !from
-      })
-      this.$analytics.setUserProperties({
-        lang: this.$route.params.lang
-      })
+      }
+      switch (to.name) {
+        case routesNames.HOME_PAGE:
+          this.$analytics.logEvent('home_page_view', {
+            ...pageData
+          })
+          break
+        case routesNames.PORTFOLIO:
+          this.$analytics.logEvent('portfolio_page_view', {
+            filters: JSON.stringify(this.$route.query),
+            ...pageData
+          })
+          break
+        case routesNames.PROJECT_PAGE:
+          this.$analytics.logEvent('project_page_view', {
+            project_id: to.params.stringId || undefined,
+            ...pageData
+          })
+          break
+        case routesNames.RESUME_PAGE:
+          this.$analytics.logEvent('resume_page_view', {
+            resume_id: to.params.resumeId || undefined,
+            ...pageData
+          })
+          this.$analytics.setUserProperties({
+            interested_in_resume: true
+          })
+          break
+        case routesNames.EDIT_RESUME_PAGE:
+          this.$analytics.logEvent('edit_resume_page_view', {
+            resume_id: to.params.resumeId || undefined,
+            query: JSON.stringify(this.$route.query),
+            ...pageData
+          })
+          break
+        case routesNames.BLOG_CONTROLLER:
+          this.$analytics.logEvent('blog_page_view', {
+            ...pageData
+          })
+          break
+        default:
+          this.$analytics.logEvent('undefined_page_view', {
+            route_name: to.name,
+            query: JSON.stringify(this.$route.query),
+            ...pageData
+          })
+          break
+      }
+      if (to?.params?.lang !== from?.params?.lang)
+        this.$analytics.setUserProperties({
+          lang: this.$route.params.lang
+        })
       if (to?.name !== from?.name)
         this.$analytics.setCurrentScreen(to?.name)
-      if (to.name === 'Resume')
-        this.$analytics.setUserProperties({
-          interested_in_resume: true
-        })
     }
         // eslint-disable-next-line no-empty
     catch (e) {}
