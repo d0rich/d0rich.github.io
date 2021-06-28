2d7528496866f3c59046af95a0abd311c57888d0
Nikolay Dorofeev <d.orich@yandex.com>
2021-06-28 17:43:14 +0700

    GA page_view events

--- src/views/LangRouter.vue
+++ src/views/LangRouter.vue
@@ -51,24 +51,24 @@ name: "LangRouter",
       }
       switch (to.name) {
         case routesNames.HOME_PAGE:
-          this.$analytics.logEvent('home_page_view', {
+          this.$analytics.logEvent('page_view', {
             ...pageData
           })
           break
         case routesNames.PORTFOLIO:
-          this.$analytics.logEvent('portfolio_page_view', {
+          this.$analytics.logEvent('page_view', {
             filters: JSON.stringify(this.$route.query),
             ...pageData
           })
           break
         case routesNames.PROJECT_PAGE:
-          this.$analytics.logEvent('project_page_view', {
+          this.$analytics.logEvent('page_view', {
             project_id: to.params.stringId || undefined,
             ...pageData
           })
           break
         case routesNames.RESUME_PAGE:
-          this.$analytics.logEvent('resume_page_view', {
+          this.$analytics.logEvent('page_view', {
             resume_id: to.params.resumeId || undefined,
             ...pageData
           })
@@ -77,19 +77,19 @@ name: "LangRouter",
           })
           break
         case routesNames.EDIT_RESUME_PAGE:
-          this.$analytics.logEvent('edit_resume_page_view', {
+          this.$analytics.logEvent('page_view', {
             resume_id: to.params.resumeId || undefined,
             query: JSON.stringify(this.$route.query),
             ...pageData
           })
           break
         case routesNames.BLOG_CONTROLLER:
-          this.$analytics.logEvent('blog_page_view', {
+          this.$analytics.logEvent('page_view', {
             ...pageData
           })
           break
         default:
-          this.$analytics.logEvent('undefined_page_view', {
+          this.$analytics.logEvent('page_view', {
             route_name: to.name,
             query: JSON.stringify(this.$route.query),
             ...pageData
