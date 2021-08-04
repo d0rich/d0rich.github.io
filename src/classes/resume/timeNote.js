6541d1d4c9ac7a11db1db7893967442d027d6ba0
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-04 23:52:34 +0700

    Time notes dates fix

--- src/classes/resume/timeNote.js
+++ src/classes/resume/timeNote.js
@@ -18,12 +18,22 @@ export class TimeNote{
             end: new Text(TimeNote.firestoreDateToString(note.period.end, 'ru-ru'),
                 TimeNote.firestoreDateToString(note.period.end, 'en-en'))
         }
+        if (this.period.begin.en === "Invalid Date")
+            this.period = {
+                begin: new Text('Ожидается', 'Expecting'),
+                end: new Text('Скоро', 'Soon')
+            }
+        if (TimeNote.firestoreDateToJsDate(note.period.end) > new Date())
+            this.period.end = new Text('Сейчас', 'Now')
         this.description = Text.fromObj(note.description)
     }
 
+    static firestoreDateToJsDate(date = { _seconds: 0, _milliseconds: 0 }){
+        return new Date(date._seconds * 1000)
+    }
     static firestoreDateToString(date = { _seconds: 0, _milliseconds: 0 },
                                  lang = ''){
-        return new Date(date._seconds * 1000)
+        return TimeNote.firestoreDateToJsDate(date)
             .toLocaleDateString(lang, { month: 'short', year: 'numeric' })
     }
 }
