68cf9eb776b0a7997bbcfb1946dbc4a9dc35246e
Nikolay Dorofeev <d.orich@yandex.com>
2021-11-17 10:40:04 +0700

    Default layout

---
+++ src/components/layout/BottomNavigation.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    <v-bottom-navigation class="bottom-nav" color="primary lighten-1">
+      <v-btn active-class="nav-btn" :to="navigation.news.route"  >
+        <span>
+          {{ navigation.news.text }}<span style="opacity: 0" class="cursor">_</span>
+        </span>
+        <v-icon>mdi-newspaper-variant</v-icon>
+      </v-btn>
+
+      <v-btn active-class="nav-btn" exact :to="navigation.resume.route"  >
+        <span>
+          {{ navigation.resume.text }}<span style="opacity: 0" class="cursor">_</span>
+        </span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+
+
+      <v-btn active-class="nav-btn" exact :to="navigation.home.route"  >
+        <span>
+          {{navigation.home.text}}<span style="opacity: 0" class="cursor">_</span>
+        </span>
+        <v-icon>mdi-console-line</v-icon>
+      </v-btn>
+
+      <v-btn active-class="nav-btn" :to="navigation.portfolio.route"  >
+        <span>
+          {{navigation.portfolio.text}}<span style="opacity: 0" class="cursor">_</span>
+        </span>
+        <v-icon>mdi-folder-star</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </nav>
+
+</template>
+
+<script>
+import {navigation} from '~/data/navigation'
+export default {
+name: "BottomNavigation",
+  data(){
+    return{
+      navigation: navigation
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bottom-nav{
+  position: fixed;
+  bottom: 0;
+  z-index: 10000;
+}
+
+</style>
