8724c0eb5b4261efbf68f5fdcaad7ab3e0a5479e
Nikolay Dorofeev <d.orich@yandex.com>
2021-11-17 12:12:25 +0700

    Home page

---
+++ src/components/pages/EnterBlock1.vue
@@ -0,0 +1,148 @@
+<template>
+  <div ref="container" v-resize="onResize"
+       class="image-container" :style="{marginLeft: mainDisplacement}">
+    <img :src="plants" alt="floor">
+    <img :src="gear_little" alt="gear_little"
+         class="float-img gear_little" :class="{disabled: disable.LittleGear}">
+    <img :src="gear_big" alt="gear_big"
+         class="float-img gear_big" :class="{disabled: disable.BigGear}">
+    <img :src="code_window" alt="code_window"
+         class="float-img code_window" :class="{disabled: disable.CodeWindow}">
+    <img :src="plants" alt="plants"
+         class="float-img plants" :class="{disabled: disable.Plants}">
+    <img :src="window" alt="window"
+         class="float-img window" :class="{disabled: disable.Window}">
+    <img :src="code_snippets" alt="code_snippets"
+         class="float-img code_snippets" :class="{disabled: disable.CodeSnippets}">
+    <img :src="code_box" alt="code_box"
+         class="float-img code_box" :class="{disabled: disable.CodeBox}">
+    <img :src="speech_bubble" alt="speech_bubble"
+         class="float-img speech_bubble" :class="{disabled: disable.SpeechBubble}">
+    <img :src="character" alt="character"
+         class="float-img character" :class="{disabled: disable.Character}">
+
+  </div>
+</template>
+
+<script>
+import character from '@/assets/home/code_fragments/character.svg'
+import code_box from '@/assets/home/code_fragments/code_box.svg'
+import code_snippets from '@/assets/home/code_fragments/code_snippets.svg'
+import code_window from '@/assets/home/code_fragments/code_window.svg'
+import floor_and_shadows from '@/assets/home/code_fragments/floor_and_shadows.svg'
+import gear_big from '@/assets/home/code_fragments/gear_big.svg'
+import gear_little from '@/assets/home/code_fragments/gear_little.svg'
+import plants from '@/assets/home/code_fragments/plants.svg'
+import speech_bubble from '@/assets/home/code_fragments/speech_bubble.svg'
+import windowP from '@/assets/home/code_fragments/window.svg'
+
+export default {
+name: "HomeEnterImage",
+  data(){
+    return{
+      character, code_box, code_snippets, code_window,
+      floor_and_shadows, gear_big, gear_little, plants,
+      speech_bubble, window: windowP,
+      mainDisplacement: 0,
+      disable:{
+        LittleGear: true,
+        BigGear: true,
+        CodeWindow: true,
+        Plants: true,
+        Window: true,
+        CodeSnippets: true,
+        CodeBox: true,
+        SpeechBubble: true,
+        Character: true
+      }
+    }
+  },
+  methods:{
+    onResize(){
+      if (this.$refs.container.offsetWidth <= window.innerWidth)
+        this.mainDisplacement = 0
+      else
+        this.mainDisplacement = `${-(this.$refs.container.offsetWidth - window.innerWidth)/2}px`
+    }
+  },
+  async mounted() {
+    await this.timeout(1000)
+    this.disable.CodeWindow = false
+    await this.timeout(200)
+    this.disable.LittleGear = false
+    this.disable.Window = false
+    await this.timeout(300)
+    this.disable.BigGear = false
+    this.disable.CodeSnippets = false
+    await this.timeout(500)
+    this.disable.CodeBox = false
+    await this.timeout(200)
+    this.disable.Character = false
+    await this.timeout(200)
+    this.disable.SpeechBubble = false
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.image-container{
+  width: 65vh;
+  position: relative;
+}
+img{
+  width: 100%;
+}
+.float-img{
+  position: absolute;
+  left: 0;
+}
+
+.gear_little{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translate(-170%, -100%);
+  }
+}
+.gear_big{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translate(-170%, -100%);
+  }
+}
+.code_window{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translateY(-100%);
+  }
+}
+.window{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translate(170%, -100%);
+  }
+}
+.code_snippets{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translate(-170%, 100%);
+  }
+}
+.code_box{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translateY(-100%);
+  }
+}
+.character{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translateX(100%);
+  }
+}
+.speech_bubble{
+  transition: all 1s ease;
+  &.disabled{
+    transform: translateY(-100%);
+  }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,292 @@
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+		<EnterBlock2 class="text-center">
+			<span style="font-size: 1.7rem;" class="mx-3">Hello, my name is Nikolay Dorofeev.</span>
+			<span style="font-size: 1.7rem;" class="ma-3">I am
+				<span style="font-size: 2.3rem; font-weight: bold">JavaScript developer<span class="cursor--active">_</span></span>
+      </span>
+			<span style="font-size: 1.7rem;" class="mx-3">You are on my personal site.</span>
+		</EnterBlock2>
 
-    <h1>Hello, world!</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+		<section class="block2 mt-16">
+			<Terminal reveal enter-word="hello world" @revealed="show.block2info = true">
+				<h1 class="align-self-start cursor-on-hover">Hello world!<span class="cursor">_</span></h1>
+				<p class="align-self-start cursor-on-hover">
+					Here you can see my
+					<router-link class="link" :to="{name: routesNames.PORTFOLIO}">portfolio</router-link>, familiarize with my
+					<router-link class="link" :to="{name: routesNames.RESUME_PAGE}">resume</router-link> , read
+					<router-link class="link" :to="{name: routesNames.NEWS_CONTROLLER}">news</router-link> from my developer life.<span class="cursor">_</span>
+				</p>
+			</Terminal>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+			<div class="block2__info">
+				<div class="info-row">
+					<img src="/img/dorich.jpg" alt="d0rich" class="border-light--accent my-photo" />
+					<div class="block2__info__text">
+						<h1>Little about me</h1>
+						<p>
+							My name is Nikolay Dorofeev, I am a JavaScript developer, now I am a 4th year student at
+							TPU in the direction of "Information systems and technologies in business and industry".
+						</p>
+						<p>
+							The acquired knowledge and personal experience of working with site layout, databases, and servers
+							is already quite enough to create Single Page Web Applications that will help you or your business
+							appear in a new light before the Internet community
+						</p>
+						<p>
+							Single Page Web Applications - sites that do not require loading separate pages.
+							This approach allows you to get a new experience with the product without unnecessary delays.
+						</p>
+						<v-btn :to="{name: routesNames.RESUME_PAGE}" color="primary">More info</v-btn>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- Секция с контактами и характеристиками -->
+		<section class="js-objects-block" style='background-image: url("/img/home/bgs/contacts-bg.jpg")'>
+			<div class="js-objects-block__contacts py-8">
+				<!-- Мои контакты -->
+				<h1>My contacts</h1>
+				<div class="contacts__btns">
+					<v-btn v-for="contact in contacts" :key="contact.text"
+								 :href="contact.href" target="_blank"
+								 class="pa-10" large
+								 tile outlined :block="!headerOn">
+            <span>
+              <v-icon>{{contact.icon}}</v-icon>  {{contact.text}}
+            </span>
+					</v-btn>
+				</div>
+			</div>
+
+			<div class="js-objects">
+				<!-- Мои характеристики программисткими объектами -->
+				<JSObjectWindow class="mb-3 mx-3"
+												name="person"
+												:object="{name: 'Nikolay', surname: 'Dorofeev', age: '21', status: 'Student'}" />
+				<JSObjectWindow class="mb-3 mx-3"
+												name="developer"
+												:object="{nickname: 'd0rich', frontend: 'Vue.js', backend: 'Node.js, Python', database: 'PostgreSQL, MongoDB'}" />
+			</div>
+		</section>
+
+		<div class="hr my-16"></div>
+
+<!--		<section class="block3">-->
+<!--			<h1 class="text-center">Last projects</h1>-->
+<!--			<transition-group name="glitch-transition" class="block3__projects">-->
+<!--				<ProjectBlock :project="project"-->
+<!--											v-for="project in projects" :key="project.id" />-->
+<!--			</transition-group>-->
+<!--			<v-btn :to="{name: routesNames.PORTFOLIO}" class="align-self-end my-5" large color="primary">-->
+<!--				{{btns.myInfo.text}}-->
+<!--				<v-icon>-->
+<!--					mdi-chevron-right-->
+<!--				</v-icon>-->
+<!--			</v-btn>-->
+<!--			<v-overlay :opacity="0" absolute :value="load.projects">-->
+<!--				<Loading />-->
+<!--			</v-overlay>-->
+<!--		</section>-->
+
+<!--		<div class="hr my-16"></div>-->
+
+<!--		<section class="block4">-->
+<!--			<h1 class="text-center">News</h1>-->
+<!--			<transition-group name="glitch-transition" class="block4__news">-->
+<!--				<news-card class="my-3" :news="news"-->
+<!--									 v-for="news in newsFeed" :key="news.id" />-->
+<!--			</transition-group>-->
+<!--			<v-btn :to="{name: routesNames.NEWS_CONTROLLER}" class="align-self-end my-5" large color="primary">-->
+<!--				{{btns.myInfo.text}}-->
+<!--				<v-icon>-->
+<!--					mdi-chevron-right-->
+<!--				</v-icon>-->
+<!--			</v-btn>-->
+<!--			<v-overlay :opacity="0" absolute :value="load.news">-->
+<!--				<Loading />-->
+<!--			</v-overlay>-->
+<!--		</section>-->
 
   </Layout>
 </template>
 
 <script>
+import Terminal from "~/components/Terminal"
+import JSObjectWindow from "~/components/pages/JSObjectWindow";
+import EnterBlock2 from "~/components/pages/EnterBlock2";
+// import ProjectBlock from "@/components/projects/ProjectBlock";
+// import NewsCard from "@/components/news/NewsCard";
+import {routesNames} from "~/data/constants";
+import { mapGetters } from 'vuex'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
+	name: 'Home',
+	components: {
+		Terminal, EnterBlock2,
+		JSObjectWindow,
+		//ProjectBlock, NewsCard
+	},
+	data(){
+		return{
+			routesNames,
+			contacts: [
+				{ href: 'mailto:d.orich@yandex.com', icon: 'mdi-email', text: 'email' },
+				{ href: 'https://t.me/d0rich', icon: 'mdi-telegram', text: 'telegram' },
+				{ href: 'https://github.com/d0rich', icon: 'mdi-github', text: 'github' },
+				{ href: 'https://vk.com/d0rich', icon: 'mdi-vk', text: 'vk' },
+			],
+			show: {
+				block2info: false
+			},
+			load: {
+				projects: true,
+				news: true
+			},
+			projects: [],
+			newsFeed: []
+		}
+	},
+	computed:{
+		...mapGetters(['headerOn'])
+	},
+	metaInfo() {
+		return {
+			title: 'Home',
+			meta: [
+				{ vmid: 'og:url', property: 'og:url', content: 'https://d0rich.github.io/'},
+			]
+		}
+	},
+
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style scoped lang="scss">
+@import "../assets/styles/font";
+.block2{
+	min-height: 400px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.block2__info{
+	margin: 2rem 0;
+	transition: opacity .5s ease;
+	.info-row{
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		margin-bottom: 2rem;
+	}
+
+	.my-photo{
+		width: 15rem;
+		margin: 0 10px 10px 0;
+	}
+	.block2__info__text{
+		margin: 0 1rem;
+	}
+
+}
+.js-objects-block{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: relative;
+	width: 100vw;
+	padding: 3rem 0;
+	background-size: cover;
+	background-attachment: fixed;
+
+	.js-objects{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+}
+.js-objects-block__contacts{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: rgba(0,0,0,.9);
+	margin-bottom: 2rem;
+	width: 100%;
+	h1{
+		padding: 0.5rem 1rem;
+		text-align: center;
+	}
+}
+.contacts__btns{
+	padding: 0.5rem 1rem;
+	max-width: 800px;
+	min-width: 300px;
+	width: 95vw;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-gap: .5rem;
+}
+.block3{
+	position: relative;
+	min-height: 400px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.block3__projects{
+	min-height: 200px;
+	min-width: 10px;
+	margin-top: 2rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: 1rem;
+	justify-items: center;
+	align-items: start;
+}
+.block4{
+	position: relative;
+	min-height: 400px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.block4__news{
+	margin: 2rem 0;
+	width: 100%;
+}
+@media screen and (max-width: 550px){
+	.block2__info{
+
+		.info-row{
+			flex-direction: column;
+			align-items: center;
+		}
+	}
+}
+@media screen and (max-width: 1080px) {
+	.block3__projects{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+}
+
+@media screen and (max-width: 640px) {
+	.block3__projects{
+		display: grid;
+		grid-template-columns: 1fr;
+	}
+	.contacts__btns{
+		display: flex;
+		flex-direction: column;
+		justify-content: unset;
+	}
 }
 </style>
+
