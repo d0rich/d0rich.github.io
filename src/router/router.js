86a9a55e0f5b454f95457a0a7ba13009f69a92bb
Nikolay Dorofeev <dorich2000@gmail.com>
2022-01-01 21:54:27 +0300

    Blog fix

--- src/router/router.js
+++ src/router/router.js
@@ -2,5 +2,6 @@ export const Router = {
     empty: './#',
     home: '/',
     blog(page = 1){return `/blog/${page > 1 ? page: ''}`},
+    tag(name, page = 1){return `/tag/${name}/${page > 1 ? page: ''}`},
     blogPost(title){ return `/blog/${title}/` }
 }
--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -3,12 +3,13 @@
     <article>
       <h1>Posts tagged {{ $page.tag.title }}</h1>
 
-      <ul>
-        <li v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" >
-          <h3><g-link :to="post.node.path">{{ post.node.title }}</g-link></h3>
-          {{ post.node.date }}
-        </li>
-      </ul>
+      <post-card v-for="post in $page.tag.belongsTo.edges" :key="post.node.id"
+                 :post="post.node"
+                 class="my-2" />
+
+      <v-pagination :length="$page.tag.belongsTo.pageInfo.totalPages"
+                    :value="$page.tag.belongsTo.pageInfo.currentPage"
+                    @input="changePage" />
     </article>
   </Layout>
 </template>
@@ -17,7 +18,7 @@
 query Tag ($id: ID!, $page: Int) {
   tag: tag (id: $id) {
     title
-    belongsTo (page: $page, perPage: 30) @paginate {
+    belongsTo (page: $page, perPage: 10) @paginate {
       totalCount
       pageInfo {
         totalPages
@@ -27,6 +28,7 @@ query Tag ($id: ID!, $page: Int) {
         node {
           ...on Post {
             title
+            image
             path
             summary
             tags {
@@ -41,7 +43,17 @@ query Tag ($id: ID!, $page: Int) {
 </page-query>
 
 <script>
+import PostCard from "../components/PostCard";
+import {Router} from "../router";
 export default {
+  components:{
+    PostCard
+  },
+  methods: {
+    changePage(page){
+      this.$router.push(Router.tag(this.$page.tag.title, page))
+    }
+  },
   metaInfo() {
     return {
       title: `Tag: ${this.$page.tag.title}`
