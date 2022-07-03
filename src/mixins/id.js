a6185ae28e22d89235993a12134e529da602afee
Nikolay Dorofeev <dorich2000@gmail.com>
2022-07-04 01:47:41 +0700

    Slugify fix

--- src/mixins/id.js
+++ src/mixins/id.js
@@ -4,7 +4,10 @@ const slugify = require('slugify')
 export const idMixin = Vue.extend({
     methods: {
         textToId(text){
-            return slugify(text)
+            return slugify(text, {
+                remove: /[*+~.()'"!:@]/g,
+                lower: true
+            })
         }
     }
 })
