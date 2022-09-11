03144968a93b5e3e7b1fa9606e64a82743bbb69f
Nikolay Dorofeev <dorich2000@gmail.com>
2022-09-11 12:10:52 +0400

    [DWS-9] Create Youtrack nodes

--- src/plugins-ts/gridsome-source-youtrack/index.ts
+++ src/plugins-ts/gridsome-source-youtrack/index.ts
@@ -5,4 +5,17 @@ import {getAllPublicAgiles, getAllPublicProjects} from './parse'
 module.exports = async function (api: PluginAPI) {
     const projects = await getAllPublicProjects()
     const agiles = await getAllPublicAgiles()
+    api.loadSource(async ({ getCollection, addCollection, store }: any) => {
+        const projectsCollection = addCollection('YoutrackProject')
+        const agilesCollection = addCollection('YoutrackAgile')
+        for (const project of projects) {
+            projectsCollection.addNode(project)
+        }
+        for (const agile of agiles) {
+            agilesCollection.addNode({
+                ...agile,
+                projects: agile.projects?.map(pr => store.createReference('YoutrackProject', pr.id))
+            })
+        }
+    })
 }
