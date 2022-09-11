13130dadcbd41d28b9f2d7fc6c07cd28a187c127
Nikolay Dorofeev <dorich2000@gmail.com>
2022-09-11 11:57:59 +0400

    Ignore js Gridsome plugins

--- src/plugins-ts/vuetify.ts
+++ src/plugins-ts/vuetify.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import colors from 'vuetify/lib/util/colors'
 
 export default {
--- src/plugins/gridsome-source-d0rich-github/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const octokit_1 = require("octokit");
-const octokit = new octokit_1.Octokit();
