2163a69ee6ffc3673e727874de1517b04f0b6c7a
Nikolay Dorofeev <dorich2000@gmail.com>
2022-10-17 21:20:50 +0400

    Add empty lines in separated method in MardownCompiler

--- scripts/notion-sync/utils/markdownCompiler.ts
+++ scripts/notion-sync/utils/markdownCompiler.ts
@@ -1,28 +1,32 @@
 export class MarkdownCompiler{
     private content: string[] = []
 
+    private addBlock(...content: string[]) {
+        this.content.push('', ...content, '')
+    }
+
     addHeading1(text: string){
-        this.content.push('', `# ${text}`, '')
+        this.addBlock(`# ${text}`)
     }
 
     addHeading2(text: string){
-        this.content.push('', `## ${text}`, '')
+        this.addBlock(`## ${text}`)
     }
 
     addHeading3(text: string){
-        this.content.push('', `### ${text}`, '')
+        this.addBlock(`### ${text}`)
     }
 
     addHeading4(text: string){
-        this.content.push('', `#### ${text}`, '')
+        this.addBlock(`#### ${text}`)
     }
 
     addParagraph(text: string){
-        this.content.push('', text, '')
+        this.addBlock(text)
     }
 
     addBlockquote(text: string){
-        this.content.push('', ...text.split('\n').map(t => `> ${t}`), '')
+        this.addBlock(...text.split('\n').map(t => `> ${t}`))
     }
 
     compile(): string {
