31506da1ce958ca7180171726b88ef67cc7a4fd9
Nikolay Dorofeev <dorich2000@gmail.com>
2022-10-19 21:28:40 +0400

    Filter blog posts to sync

--- scripts/notion-sync/blog/index.ts
+++ scripts/notion-sync/blog/index.ts
@@ -8,8 +8,17 @@ export async function syncBlogPosts(notion: Client) {
     const db = await notion.databases.query({ database_id: DATABASE_ID })
     const postsPromises = db.results.map(async (item) => fetchPage(item.id, notion))
     const posts = await Promise.all(postsPromises)
+    const postsFiltered = posts.filter(post => {
+        if (post.meta.properties?.date?.type !== 'date')
+            return false
+        if (new Date(post.meta.properties?.date?.date?.start || Number(new Date()) + 1000) > new Date())
+            return false
+        if (post.meta.properties?.title?.type !== 'title')
+            return false
+        return true
+    })
     const saver = new NotionPageSaver('content/notion/test', 'blogPost')
-    for (let post of posts) {
+    for (let post of postsFiltered) {
         await saver.save(post)
     }
 }
