c9cccb3248e6cc68ce1910706440450772b34fe4
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-19 21:56:30 +0400

    Color mode

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script>
+export default defineComponent({
+  name: 'App',
+  setup(){
+    definePageMeta({
+      colorMode: 'dark'
+    })
+  }
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {}
+      ]
+    }
+  },
+  css: [
+    'assets/css/theme.css'
+  ],
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
     '@morev/vue-transitions/nuxt'
-  ]
+  ],
+  colorMode: {
+    classSuffix: '',
+    
+  }
+  
 })
