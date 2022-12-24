373632045d3e477762c578e4c89d79762010c8aa
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-24 16:11:35 +0400

    Highlight component

--- components/DBtn.vue
+++ components/DBtn.vue
@@ -1,51 +1,20 @@
 <template>
-  <NuxtLink to="/" class="btn overflow-visible">
-    <div class="btn-hihlighter" />
-    <slot />
+  <NuxtLink to="/" class="btn">
+    <Highlight>
+      <slot />
+    </Highlight>
   </NuxtLink>
 </template>
 
-<style>
-.btn{
-  @apply font-semibold relative -rotate-6 uppercase;
-}
+<script lang="ts">
 
-.btn > .btn-hihlighter {
-  width: 0%;
-  height: 0%;
-  top: 50%;
-  left: 50%;
-  content: '';
-  opacity: 0;
-  @apply absolute backdrop-invert transition-all;
-}
+import Highlight from './wrapper/Highlight.vue';
 
-.btn:hover > .btn-hihlighter, .btn:focus > .btn-hihlighter{
-  width: 110%;
-  height: 130%;
-  top: -15%;
-  left: -5%;
-  opacity: 1;
-  animation: hl-rotate linear 1s infinite;
-}
-
-.btn:focus > .btn-hihlighter{
-  width: 140%;
-  height: 200%;
-  top: -50%;
-  left: -20%;
-  opacity: 1;
-  animation: hl-rotate linear 1s infinite;
-}
-
-@keyframes hl-rotate {
-  0, 100% {
-    transform: skew(0deg) rotate(12deg);
+export default defineComponent({
+  name: 'Button',
+  components: {
+    Highlight
   }
-  50% {
-    transform: skew(-40deg) rotate(-28deg);
-  }
-}
+})
 
-</style>
-
+</script>
---
+++ components/wrapper/Highlight.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="btn">
+    <div class="btn-hihlighter" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+
+export default defineComponent({
+  name: 'Highlight'
+})
+
+</script>
+
+<style>
+.btn{
+  @apply font-semibold relative -rotate-6 uppercase;
+}
+
+.btn > .btn-hihlighter {
+  width: 0%;
+  height: 0%;
+  top: 50%;
+  left: 50%;
+  content: '';
+  opacity: 0;
+  @apply absolute backdrop-invert transition-all;
+}
+
+.btn:hover > .btn-hihlighter, .btn:focus .btn-hihlighter{
+  width: 110%;
+  height: 130%;
+  top: -15%;
+  left: -5%;
+  opacity: 1;
+  animation: hl-rotate linear 1s infinite;
+}
+
+.btn:focus .btn-hihlighter{
+  width: 140%;
+  height: 200%;
+  top: -50%;
+  left: -20%;
+  opacity: 1;
+  animation: hl-rotate linear 1s infinite;
+}
+
+@keyframes hl-rotate {
+  0, 100% {
+    transform: skew(0deg) rotate(12deg);
+  }
+  50% {
+    transform: skew(-40deg) rotate(-28deg);
+  }
+}
+</style>
