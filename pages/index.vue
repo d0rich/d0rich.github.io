0f3d368641ce132df9ef620731205616622f25cb
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-26 10:10:36 +0400

    Home page draft

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Home page
+    <section id="home-intro-section">
+
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default defineComponent({
+  name: 'HomePage',
+
+})
+
+</script>
+
+<style>
+
+</style>
