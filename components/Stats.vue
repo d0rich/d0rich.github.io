afdf30235458a19e04d182dab7d02e38acb0cbb1
Nikolay Dorofeev <dorich2000@gmail.com>
2023-02-12 18:10:38 +0400

    Stats implementation

--- components/Stats.vue
+++ components/Stats.vue
@@ -1,11 +1,66 @@
 <template>
   <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-    <polygon v-for="scale, index in scales" :key="index" 
-          :points="pointsToString(shrinkStar(fullStarPoints, scale))" 
+    <polygon v-for="star, index in scaledStars" :key="index" 
+          :points="pointsToString(star)" 
           :class="{
             'fill-neutral-500': index % 2,
             'fill-neutral-800': !(index % 2),
           }"/>
+    <!-- 1 -->
+    <polygon :points="pointsToString([ 
+        starFromValue(firstStat.value)[posToIndex['1']],
+        starFromValue(Math.min(firstStat.value, fifthStat.value))[posToIndex['5-1']],
+        center
+      ])" class="fill-yellow-500" />
+    <polygon :points="pointsToString([ 
+        starFromValue(firstStat.value)[posToIndex['1']],
+        starFromValue(Math.min(firstStat.value, secondStat.value))[posToIndex['1-2']],
+        center
+      ])"  class="fill-yellow-700" />
+    <!-- 2 -->
+    <polygon :points="pointsToString([ 
+        starFromValue(secondStat.value)[posToIndex['2']],
+        starFromValue(Math.min(firstStat.value, secondStat.value))[posToIndex['1-2']],
+        center
+      ])" class="fill-yellow-500" />
+    <polygon :points="pointsToString([ 
+        starFromValue(secondStat.value)[posToIndex['2']],
+        starFromValue(Math.min(thirdStat.value, secondStat.value))[posToIndex['2-3']],
+        center
+      ])"  class="fill-yellow-700" />
+    <!-- 3 -->
+    <polygon :points="pointsToString([ 
+        starFromValue(thirdStat.value)[posToIndex['3']],
+        starFromValue(Math.min(secondStat.value, thirdStat.value))[posToIndex['2-3']],
+        center
+      ])" class="fill-yellow-500" />
+    <polygon :points="pointsToString([ 
+        starFromValue(thirdStat.value)[posToIndex['3']],
+        starFromValue(Math.min(thirdStat.value, forthStat.value))[posToIndex['3-4']],
+        center
+      ])"  class="fill-yellow-700" />
+    <!-- 4 -->
+    <polygon :points="pointsToString([ 
+        starFromValue(forthStat.value)[posToIndex['4']],
+        starFromValue(Math.min(thirdStat.value, forthStat.value))[posToIndex['3-4']],
+        center
+      ])" class="fill-yellow-500" />
+    <polygon :points="pointsToString([ 
+        starFromValue(forthStat.value)[posToIndex['4']],
+        starFromValue(Math.min(forthStat.value, fifthStat.value))[posToIndex['4-5']],
+        center
+      ])"  class="fill-yellow-700" />
+    <!-- 5 -->
+    <polygon :points="pointsToString([ 
+        starFromValue(fifthStat.value)[posToIndex['5']],
+        starFromValue(Math.min(forthStat.value, fifthStat.value))[posToIndex['4-5']],
+        center
+      ])" class="fill-yellow-500" />
+    <polygon :points="pointsToString([ 
+        starFromValue(fifthStat.value)[posToIndex['5']],
+        starFromValue(Math.min(fifthStat.value, firstStat.value))[posToIndex['5-1']],
+        center
+      ])"  class="fill-yellow-700" />
   </svg>
 </template>
 
@@ -13,7 +68,11 @@
 
 export default defineComponent({
   name: 'Stats',
-  setup(){
+  props: {
+    titles: Array as () => string[],
+    values: Array as () => number[]
+  },
+  setup(props){
     const fullStarPoints = ref<number[][]>([
       [500,0], // 1
       [610,350], // 1-2
@@ -26,21 +85,54 @@ export default defineComponent({
       [20,350], // 5
       [390,350] // 5-1
     ])
-
-    return {
-      fullStarPoints,
-      scales: ref<number[]>([1, 0.8, 0.6, 0.4, 0.2]),
-      pointsToString(points: number[][]){
-        return points.map(pair => pair.join(',')).join(' ')
-      },
-      shrinkStar(points: number[][], scale: number, cx = 500, cy = 500){
-        return points.map(coords => {
-          return [
-            coords[0] - (coords[0] - cx) * (1 - scale),
-            coords[1] - (coords[1] - cy) * (1 - scale),
-          ]
-        })
+    const scales = ref<number[]>([1, 0.8, 0.6, 0.4, 0.2])
+    const posToIndex = {
+      '1': 0,
+      '1-2': 1,
+      '2': 2,
+      '2-3': 3,
+      '3': 4,
+      '3-4': 5,
+      '4': 6,
+      '4-5': 7,
+      '5': 8,
+      '5-1': 9
+    }
+    const center = [500, 500]
+    function pointsToString(points: number[][]){
+      return points.map(pair => pair.join(',')).join(' ')
+    }
+    function shrinkStar(points: number[][], scale: number, cx = 500, cy = 500){
+      return points.map(coords => {
+        return [
+          coords[0] - (coords[0] - cx) * (1 - scale),
+          coords[1] - (coords[1] - cy) * (1 - scale),
+        ]
+      })
+    }
+    const scaledStars = computed(() => {
+      return scales.value.map((scale) => shrinkStar(fullStarPoints.value, scale))
+    })
+    function starFromValue(value: number) {
+      return scaledStars.value[5 - value]
+    }
+    function statFromIndex(index: number) {
+      return {
+        value: props.values ? props.values[index] ?? 1 : 1,
+        title: props.titles ? props.titles[index] ?? '' : ''
       }
+    }
+    return {
+      fullStarPoints, posToIndex, center,
+      scales, scaledStars,
+      starFromValue,
+      pointsToString,
+      shrinkStar,
+      firstStat: computed(() => statFromIndex(0)),
+      secondStat: computed(() => statFromIndex(1)),
+      thirdStat: computed(() => statFromIndex(2)),
+      forthStat: computed(() => statFromIndex(3)),
+      fifthStat: computed(() => statFromIndex(4)),
     }
   }
 })
