e7374114c4c45c03f69117b2d23947f5b30cea0b
Nikolay Dorofeev <dorich2000@gmail.com>
2023-02-19 21:46:24 +0400

    Projects placeholder

---
+++ pages/projects.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Projects
+  </div>
+</template>
+
+<script lang="ts">
+
+export default defineComponent({
+  setup(){
+    useHead({
+      title: 'Projects'
+    })
+  }
+})
+
+</script>
