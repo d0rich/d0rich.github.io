8814b886e7df69d04224fb942579dc85efdc6211
Nikolay Dorofeev <dorich2000@gmail.com>
2023-02-25 21:36:20 +0400

    Blog draft

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ContentDoc tag="article" class="blog-article" />
+  </div>
+</template>
+
+<script lang="ts">
+
+export default defineComponent({
+  setup(){
+    
+  }
+})
+
+</script>
+
+<style>
+.blog-article{
+  @apply prose dark:prose-invert font-sans
+         mx-auto px-3 sm:px-0;
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { NavItem, QueryBuilderParams, ParsedContent } from '@nuxt/content/dist/runtime/types';
+const blogQuery: QueryBuilderParams = {
+  path: '/blog',
+  without: [ 'excerpt', 'body' ]
+  // where: [
+  //   { _path: /^\/blog/ }
+  // ]
+}
+
+</script>
+
+<template>
+  <div>
+    Blog
+
+    <nav>
+      <ContentList :query="blogQuery" v-slot="{ list }: { list: ParsedContent[] }">
+        <NuxtLink v-for="article in list" :key="article._path" :to="article._path">
+          <div>
+            <h2>{{ article.title }}</h2>
+            <p>{{ article.description }}</p>
+          </div>
+        </NuxtLink>
+      </ContentList>
+    </nav>
+  </div>
+</template>
