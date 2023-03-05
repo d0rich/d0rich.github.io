0efdc37978c8eb81c151e5442f623b8224f033af
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-05 22:29:17 +0400

    Styles for blog

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,27 +1,26 @@
 <template>
-  <div>
+  <div class="pb-[50vh] pt-10">
     <ContentDoc  v-slot="{ doc }">
       <div class="blog-article">
-        <img :src="doc.image" />
+        <time v-if="doc.date">{{ dateToDayMonthYear(doc.date) }}</time>
       </div>
       <ContentRenderer :value="doc" tag="article" class="blog-article" />
     </ContentDoc>
   </div>
 </template>
 
-<script lang="ts">
-
-export default defineComponent({
-  setup(){
-    
-  }
-})
-
-</script>
-
 <style>
 .blog-article{
   @apply prose dark:prose-invert font-sans
-         mx-auto px-3 sm:px-0;
+         md:prose-lg
+         mx-auto px-3 sm:px-0
+         dark:prose-headings:text-cyan-400 dark:prose-a:text-inherit
+         dark:prose-p:first-letter:bg-cyan-800
+         dark:prose-blockquote:border-cyan-400
+         dark:marker:text-cyan-400;
+}
+
+.blog-article blockquote p::before {
+  content: none;
 }
 </style>
