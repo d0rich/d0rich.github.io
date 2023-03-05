975e6341ba701015845671b3fbfe1d9d5940adbc
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-05 23:26:26 +0400

    Placeholder for projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: 'Projects'
+})
+</script>
+
 <template>
   <div>
-    Projects
+    <div class="relative isolate px-3 max-w-3xl mx-auto my-10 overflow-hidden">
+      <div class="max-w-lg">
+        <h1 class="text-6xl sm:text-8xl font-serif mb-5 text-red-300">Projects</h1>
+        <p class="first-letter:bg-red-600">
+          Work in progress.
+        </p>
+        <p>
+          Docs template for projects will be created. Then all projects docs websites will be aggregated on this page.
+        </p>
+      </div>
+      <Mask mask="spider" color outline 
+        class="h-full absolute top-0 right-0 -z-10 brightness-[25%] sm:brightness-100 transition-all" />
+    </div>
   </div>
 </template>
-
-<script lang="ts">
-
-export default defineComponent({
-  setup(){
-    useHead({
-      title: 'Projects'
-    })
-  }
-})
-
-</script>
