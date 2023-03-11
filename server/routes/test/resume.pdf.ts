e7a84e4ca9bafd940a694dc81e212816a9d7a7fe
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-11 20:06:47 +0400

    Delete PDFkit

--- server/routes/test/resume.pdf.ts
+++ server/routes/test/resume.pdf.ts
@@ -1,9 +1,4 @@
-import PDFGenerator from 'pdfkit'
-
 export default defineEventHandler((event) => {
-  const doc = new PDFGenerator()
-  doc.pipe(event.node.res)
-  doc.text('Some awesome example text')
-  doc.end()
+  
   setResponseStatus(200)
 })
