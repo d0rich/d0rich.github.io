c4ea8103a635b4e12b6611bebaacdd3ff8c8e008
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-11 15:26:57 +0400

    Fix first page transition bug

--- middleware/switch-transition.global.ts
+++ middleware/switch-transition.global.ts
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+  // If first page in session
+  if (to.fullPath == from.fullPath) return
   const sectionBefore = from.path.split('/')[1]
   const sectionAfter = to.path.split('/')[1]
   if (sectionBefore === sectionAfter)
