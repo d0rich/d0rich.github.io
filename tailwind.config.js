0b829a3d55119800d11c1ee6543ebdc6eafbd6ff
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-19 21:01:25 +0400

    Import tailwind config as preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const d0richDesignSystem = require('@d0rich/nuxt-design-system/tailwind.config')
-
 /** @type {import('tailwindcss').Config} */
-module.exports = d0richDesignSystem
+module.exports = {
+  presets: [require('@d0rich/nuxt-design-system/tailwind.config')]
+}
