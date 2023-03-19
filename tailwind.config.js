a8ec9e671842c9212b358e6d751a5657e935fcbe
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-19 15:34:25 +0400

    Import tailwindcss config for intellisense

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const d0richDesignSystem = require('../nuxt-design-system/tailwind.config')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = d0richDesignSystem
