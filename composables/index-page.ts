7cf06beae95ca1371f36845ef1b181de1e318f3c
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-27 18:16:27 +0000

    Homepage animation refactoring (#30)
    * Refactor homepage animations
    
    * useSafeOnMounted
    
    * Return old lines definition
    
    * Update name of joob in workflow
    
    * Fix path comparison during transition

--- composables/index-page.ts
+++ composables/index-page.ts
@@ -1,10 +1,13 @@
-import { gsap } from '@d0rich/nuxt-design-system/exports'
+import gsap from 'gsap'
 import { ComponentPublicInstance } from 'vue'
+import { LineEdge } from '@d0rich/nuxt-design-system/types'
 
-const { generatePolygonLineKeyframes, generatePolygonPointsKeyframes } =
+const { applyLinePerPointAnimation, applyStaticPoints } =
   usePolygonAnimationUtils()
 
-export const useIntroBlockAnimation = () => {
+const { getAbsoluteBoundingsGetters } = useElementsUtils()
+
+export const useIntroBlockAnimation = (rootRef: Ref<HTMLElement>) => {
   const nodes = {
     section: ref<Element | null>(null),
     main: ref<Element | null>(null),
@@ -15,7 +18,7 @@ export const useIntroBlockAnimation = () => {
   }
 
   // Transition animation
-  onMounted(() => {
+  useSafeOnMounted(rootRef, () => {
     const introScrollTrigger = {
       scrub: 1,
       start: () => 0,
@@ -52,27 +55,29 @@ export const useIntroBlockAnimation = () => {
   })
 
   // Line animation
-  onMounted(() => {
-    const lineScrollTrigger = {
-      scrub: 1,
-      end: () => window.innerHeight * 1,
-      start: () => window.innerHeight * 0.3
-    }
-    generatePolygonPointsKeyframes([
-      { left: { x: 90, y: 0 }, right: { x: 95, y: 0 } },
-      { left: { x: 50, y: 33 }, right: { x: 63, y: 35 } },
-      { left: { x: 80, y: 58 }, right: { x: 85, y: 55 } },
-      { left: { x: 50, y: 100 }, right: { x: 75, y: 100 } }
-    ]).forEach((kfs) => {
-      const point = nodes.svg.value?.createSVGPoint()
-      if (point) {
-        nodes.line.value?.points.appendItem(point)
+  useSafeOnMounted(rootRef, () => {
+    if (!nodes.svg.value || !nodes.line.value) return
+
+    applyLinePerPointAnimation(
+      [
+        { left: { x: 90, y: 0 }, right: { x: 95, y: 0 } },
+        { left: { x: 50, y: 33 }, right: { x: 63, y: 35 } },
+        { left: { x: 80, y: 58 }, right: { x: 85, y: 55 } },
+        { left: { x: 50, y: 100 }, right: { x: 75, y: 100 } }
+      ],
+      nodes.svg.value,
+      nodes.line.value,
+      (point, keyframes) => {
         gsap.to(point, {
-          keyframes: kfs,
-          scrollTrigger: lineScrollTrigger
+          keyframes,
+          scrollTrigger: {
+            scrub: 1,
+            end: () => window.innerHeight * 1,
+            start: () => window.innerHeight * 0.3
+          }
         })
       }
-    })
+    )
   })
 
   return {
@@ -80,7 +85,7 @@ export const useIntroBlockAnimation = () => {
   }
 }
 
-export const useSectionsDescriptionAnimation = () => {
+export const useSectionsDescriptionAnimation = (rootRef: Ref<HTMLElement>) => {
   const nodes = {
     svg: ref<(SVGElement & SVGSVGElement) | null>(null),
     line: ref<SVGPolygonElement | null>(null),
@@ -91,76 +96,56 @@ export const useSectionsDescriptionAnimation = () => {
   let observer: IntersectionObserver
   const currentSection = ref<'portfolio' | 'blog' | 'resume' | null>(null)
   // Line animation
-  onMounted(() => {
-    // TODO: delete timeout when https://github.com/nuxt/nuxt/issues/13471 is fixed
-    setTimeout(() => {
-      const scrollTrigger = {
-        scrub: 1,
-        start: () =>
-          (nodes.svg?.value?.getBoundingClientRect().top ?? 0) +
-          window.scrollY -
-          window.innerHeight *
-            (1 - 0.35 * Number(window.innerHeight < window.innerWidth)),
-        end: () =>
-          (nodes.svg?.value?.getBoundingClientRect().bottom ?? 0) +
-          window.scrollY -
-          window.innerHeight *
-            (1 - 0.35 * Number(window.innerHeight < window.innerWidth))
+  useSafeOnMounted(rootRef, () => {
+    const svgBoundings = getAbsoluteBoundingsGetters(nodes.svg, {
+      y: () =>
+        -window.innerHeight *
+        (1 - 0.35 * Number(window.innerHeight < window.innerWidth))
+    })
+    const scrollTrigger = {
+      scrub: 1,
+      start: svgBoundings.top,
+      end: svgBoundings.bottom
+    }
+    const line: LineEdge[] = [
+      { left: { x: 10, y: 0 }, right: { x: 13, y: 0 } },
+      { left: { x: 87, y: 4 }, right: { x: 90, y: 1 } },
+      { left: { x: 83, y: 16 }, right: { x: 87, y: 10 } },
+      { left: { x: 97, y: 6 }, right: { x: 94, y: 7 } },
+      { left: { x: 43, y: 8 }, right: { x: 50, y: 9 } },
+      { left: { x: 45, y: 67 }, right: { x: 57, y: 49 } },
+      { left: { x: 90, y: 40 }, right: { x: 85, y: 38 } },
+      { left: { x: 83, y: 23 }, right: { x: 72, y: 14 } },
+      { left: { x: 95, y: 28 }, right: { x: 98, y: 25 } },
+      { left: { x: 97, y: 93 }, right: { x: 99, y: 99 } },
+      { left: { x: 40, y: 76 }, right: { x: 30, y: 75 } },
+      { left: { x: 80, y: 60 }, right: { x: 95, y: 50 } },
+      { left: { x: 50, y: 100 }, right: { x: 75, y: 100 } }
+    ]
+    if (!nodes.svg.value || !nodes.line.value) return
+    applyLinePerPointAnimation(
+      line,
+      nodes.svg.value,
+      nodes.line.value,
+      (point, keyframes) => {
+        gsap.to(point, {
+          keyframes,
+          scrollTrigger
+        })
       }
-      const line = [
-        { left: { x: 10, y: 0 }, right: { x: 13, y: 0 } },
-        { left: { x: 87, y: 4 }, right: { x: 90, y: 1 } },
-        { left: { x: 83, y: 16 }, right: { x: 87, y: 10 } },
-        { left: { x: 97, y: 6 }, right: { x: 94, y: 7 } },
-        { left: { x: 43, y: 8 }, right: { x: 50, y: 9 } },
-        { left: { x: 45, y: 67 }, right: { x: 57, y: 49 } },
-        { left: { x: 90, y: 40 }, right: { x: 85, y: 38 } },
-        { left: { x: 83, y: 23 }, right: { x: 72, y: 14 } },
-        { left: { x: 95, y: 28 }, right: { x: 98, y: 25 } },
-        { left: { x: 97, y: 93 }, right: { x: 99, y: 99 } },
-        { left: { x: 40, y: 76 }, right: { x: 30, y: 75 } },
-        { left: { x: 80, y: 60 }, right: { x: 95, y: 50 } },
-        { left: { x: 50, y: 100 }, right: { x: 75, y: 100 } }
-      ]
-      generatePolygonPointsKeyframes(line).forEach((kfs) => {
-        const point = nodes.svg.value?.createSVGPoint()
-        if (point) {
-          nodes.line.value?.points.appendItem(point)
-          gsap.to(point, {
-            keyframes: kfs,
-            scrollTrigger
-          })
-        }
-      })
-    }, 1000)
+    )
   })
   // Current section
-  onMounted(() => {
+  useSafeOnMounted(rootRef, () => {
     observer = new IntersectionObserver(
       (entries) => {
-        if (
-          entries.some(
-            (entry) =>
-              entry.isIntersecting && entry.target === nodes.resume.value
-          )
-        ) {
+        const checkIntersection = (el: Element | null) =>
+          entries.some((entry) => entry.isIntersecting && entry.target === el)
+        if (checkIntersection(nodes.resume.value)) {
           currentSection.value = 'resume'
-          return
-        }
-        if (
-          entries.some(
-            (entry) => entry.isIntersecting && entry.target === nodes.blog.value
-          )
-        ) {
+        } else if (checkIntersection(nodes.blog.value)) {
           currentSection.value = 'blog'
-          return
-        }
-        if (
-          entries.some(
-            (entry) =>
-              entry.isIntersecting && entry.target === nodes.portfolio.value
-          )
-        ) {
+        } else if (checkIntersection(nodes.portfolio.value)) {
           currentSection.value = 'portfolio'
         }
       },
@@ -182,7 +167,7 @@ export const useSectionsDescriptionAnimation = () => {
   }
 }
 
-export const useStoryAnimation = () => {
+export const useStoryAnimation = (rootRef: Ref<HTMLElement>) => {
   const nodes = {
     storyContainer: ref<Element | null>(null),
     svg: ref<(SVGElement & SVGSVGElement) | null>(null),
@@ -190,56 +175,43 @@ export const useStoryAnimation = () => {
     linePlaceholder: ref<SVGPolygonElement | null>(null)
   }
 
-  onMounted(() => {
-    // TODO: delete timeout when https://github.com/nuxt/nuxt/issues/13471 is fixed
-    setTimeout(() => {
-      const scrollTrigger = {
-        scrub: 1,
-        start: () =>
-          (nodes.storyContainer?.value?.getBoundingClientRect().top ?? 0) +
-          window.scrollY -
-          window.innerHeight,
-        end: () =>
-          (nodes.storyContainer?.value?.getBoundingClientRect().bottom ?? 0) +
-          window.scrollY -
-          window.innerHeight
-      }
-      const line = [
-        { left: { x: 2, y: 0 }, right: { x: 7, y: 0 } },
-        { left: { x: 3, y: 10 }, right: { x: 9, y: 13 } },
-        { left: { x: 1, y: 20 }, right: { x: 7, y: 20 } },
-        { left: { x: 2, y: 30 }, right: { x: 7, y: 30 } },
-        { left: { x: 0, y: 36 }, right: { x: 6, y: 40 } },
-        { left: { x: 3, y: 47 }, right: { x: 8, y: 50 } },
-        { left: { x: 1, y: 64 }, right: { x: 6, y: 60 } },
-        { left: { x: 4, y: 70 }, right: { x: 9, y: 73 } },
-        { left: { x: 2, y: 80 }, right: { x: 6, y: 85 } },
-        { left: { x: 3, y: 92 }, right: { x: 8, y: 90 } },
-        { left: { x: 0, y: 100 }, right: { x: 7, y: 100 } }
-      ]
-      // Scroll background from last frame
-      generatePolygonLineKeyframes(line)
-        .at(-1)
-        ?.forEach((coords) => {
-          const point = nodes.svg.value?.createSVGPoint()
-          if (point) {
-            point.x = coords.x
-            point.y = coords.y
-            nodes.linePlaceholder.value?.points.appendItem(point)
+  useSafeOnMounted(rootRef, () => {
+    const line: LineEdge[] = [
+      { left: { x: 2, y: 0 }, right: { x: 7, y: 0 } },
+      { left: { x: 3, y: 10 }, right: { x: 9, y: 13 } },
+      { left: { x: 1, y: 20 }, right: { x: 7, y: 20 } },
+      { left: { x: 2, y: 30 }, right: { x: 7, y: 30 } },
+      { left: { x: 0, y: 36 }, right: { x: 6, y: 40 } },
+      { left: { x: 3, y: 47 }, right: { x: 8, y: 50 } },
+      { left: { x: 1, y: 64 }, right: { x: 6, y: 60 } },
+      { left: { x: 4, y: 70 }, right: { x: 9, y: 73 } },
+      { left: { x: 2, y: 80 }, right: { x: 6, y: 85 } },
+      { left: { x: 3, y: 92 }, right: { x: 8, y: 90 } },
+      { left: { x: 0, y: 100 }, right: { x: 7, y: 100 } }
+    ]
+    if (!nodes.svg.value || !nodes.linePlaceholder.value || !nodes.line.value)
+      return
+    // Scroll background from last frame
+    applyStaticPoints(line, nodes.svg.value, nodes.linePlaceholder.value)
+    // Scroll thumb animation
+    const storyBoundings = getAbsoluteBoundingsGetters(nodes.storyContainer, {
+      y: () => -window.innerHeight
+    })
+    applyLinePerPointAnimation(
+      line,
+      nodes.svg.value,
+      nodes.line.value,
+      (point, keyframes) => {
+        gsap.to(point, {
+          keyframes,
+          scrollTrigger: {
+            scrub: 1,
+            start: storyBoundings.top,
+            end: storyBoundings.bottom
           }
         })
-      // Scroll thumb animation
-      generatePolygonPointsKeyframes(line).forEach((kfs) => {
-        const point = nodes.svg.value?.createSVGPoint()
-        if (point) {
-          nodes.line.value?.points.appendItem(point)
-          gsap.to(point, {
-            keyframes: kfs,
-            scrollTrigger
-          })
-        }
-      })
-    }, 1000)
+      }
+    )
   })
 
   return {
