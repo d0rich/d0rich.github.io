0cedbd6ab1dc6714eadbe9448483e7fe47c59966
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-27 22:13:54 +0400

    Fix path comparison during transition

--- middleware/switch-transition.global.ts
+++ middleware/switch-transition.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   // If first page in session
-  if (to.fullPath === from.fullPath) return
+  if (to.path === from.path) return
   const sectionBefore = from.path.split('/')[1]
   const sectionAfter = to.path.split('/')[1]
   if (sectionBefore === sectionAfter) to.meta.pageTransition = false
