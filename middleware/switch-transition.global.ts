c5dbec27c2bad31903f57949776dd23bc4935a8f
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-28 16:04:39 +0400

    Tranisition middleware fix

--- middleware/switch-transition.global.ts
+++ middleware/switch-transition.global.ts
@@ -1,7 +1,26 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   // If first page in session
-  if (to.path === from.path) return
+  if (to.fullPath === from.fullPath) return
+  // If local navigation
+  if (
+    to.path === from.path &&
+    (to.fullPath.includes('#') || from.fullPath.includes('#'))
+  )
+    return
   const sectionBefore = from.path.split('/')[1]
   const sectionAfter = to.path.split('/')[1]
-  if (sectionBefore === sectionAfter) to.meta.pageTransition = false
+  if (sectionBefore === sectionAfter) {
+    from.meta.pageTransition = {
+      name: 'section',
+      leaveActiveClass: '',
+      leaveFromClass: '',
+      leaveToClass: ''
+    }
+    to.meta.pageTransition = {
+      name: 'section',
+      enterActiveClass: '',
+      enterFromClass: '',
+      enterToClass: ''
+    }
+  }
 })
