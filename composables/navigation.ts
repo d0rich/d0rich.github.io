d9247ece324a4f3b66bdc334c534820010f77dd0
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-09 15:43:28 +0400

    lintfix

--- composables/navigation.ts
+++ composables/navigation.ts
@@ -2,7 +2,9 @@ import { QueryBuilderWhere } from '@nuxt/content/dist/runtime/types'
 
 export const useBlogNavigationConfig = () => {
   const config = useRuntimeConfig()
-  const filterObject: QueryBuilderWhere = config.public.isProd ? { _draft: { $not: true } } : {}
+  const filterObject: QueryBuilderWhere = config.public.isProd
+    ? { _draft: { $not: true } }
+    : {}
   return {
     itemsOnPage: 6,
     filter: filterObject
