aefe28f7df55d0fc58338dff244c4b1e059f23b3
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-06 15:55:52 +0400

    Intro block as component

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,11 @@
 <script setup lang="ts">
 import { ParsedContent } from '@nuxt/content/dist/runtime/types'
-import {
-  CharacterPose,
-  MaskType,
-  ActionFanItem
-} from '@d0rich/nuxt-design-system/types'
+import { MaskType } from '@d0rich/nuxt-design-system/types'
 
 // Use animation composables
-const currentPose = ref<CharacterPose>('idle')
 const rootRef = ref<HTMLElement>() as Ref<HTMLElement>
-const {
-  introNodeRefs,
-  sectionsNodeRefs,
-  currentSection,
-  skillsNodeRefs,
-  storyNodeRefs
-} = useHomepageAnimations(rootRef)
-
-// Intro block
-const socialLinks = ref([
-  { icon: 'ic:sharp-mail', link: 'mailto:dorich2000@gmail.com' },
-  { icon: 'fa6-brands:linkedin', link: 'https://www.linkedin.com/in/d0rich/' },
-  { icon: 'fa6-brands:github', link: 'https://github.com/d0rich' },
-  { icon: 'fa6-brands:telegram', link: 'https://d0rich.t.me/' }
-])
-const actions: ActionFanItem<CharacterPose>[] = [
-  {
-    title: 'Sections',
-    emit: 'action',
-    attrs: {
-      href: '#sections',
-      noPassiveHighlight: true
-    }
-  },
-  {
-    title: 'Skills',
-    emit: 'profi',
-    attrs: {
-      href: '#skills',
-      noPassiveHighlight: true
-    }
-  },
-  {
-    title: 'Story',
-    emit: 'idle',
-    attrs: {
-      href: '#story',
-      noPassiveHighlight: true
-    }
-  }
-]
+const { sectionsNodeRefs, currentSection, skillsNodeRefs, storyNodeRefs } =
+  useHomepageAnimations(rootRef)
 
 // Sections block
 const sectionsLineColor = computed(() => {
@@ -98,98 +54,7 @@ const { data } = useAsyncData('homepage', async () => {
   <div v-if="data" ref="rootRef" class="-mt-20">
     <AsyncSafeSeoWithOg og-title="Homepage" />
     <!-- Intro block -->
-    <section
-      :ref="(el) => { introNodeRefs.section.value = el as Element }"
-      style="height: 200vh"
-    >
-      <div class="sticky top-0">
-        <div
-          :ref="(el) => { introNodeRefs.main.value = el as Element }"
-          class="relative w-screen max-w-full h-screen"
-        >
-          <div
-            :ref="(el) => { introNodeRefs.bg.value = el as Element }"
-            class="absolute h-full w-full top-0 left-0 bg-[url('~/assets/img/bg/d-bw.webp')] bg-cover bg-center z-[2]"
-          />
-          <DWrapShape
-            :ref="
-              (el) => {
-                introNodeRefs.text.value = componentFromNodeRef(el)
-              }
-            "
-            class="absolute w-fit top-1/3 left-0 right-0 mx-auto z-[3]"
-            shape-class="intro-shape"
-          >
-            <ContentRenderer
-              tag="div"
-              :value="data.intro"
-              class="p-10 text-xl font-serif text-center [&>p>br]:!m-0 [&>p]:!m-0"
-            />
-          </DWrapShape>
-        </div>
-        <DWrapBackground
-          dots
-          :dots-style="{ clipPath: 'polygon(20% 100%, 100% 100%, 100% 0%)' }"
-          class="bg-[url('~/assets/img/bg/it-office.png')] bg-cover bg-center"
-          overlay-class="backdrop-saturate-0 bg-green-900 bg-opacity-90"
-        >
-          <template #svg>
-            <DAnimationHypnosis
-              class="absolute landscape:h-full m-auto inset-0"
-              path-class="fill-white"
-            />
-            <svg
-              :ref="(el) => { introNodeRefs.svg.value = el as (SVGElement & SVGSVGElement) | null }"
-              height="100%"
-              width="100%"
-              class="absolute inset-0 w-full max-w-6xl mx-auto h-full sharp-shadow ss-r-4 ss-b-2 ss-neutral-900"
-              viewBox="80 0 20 100"
-              preserveAspectRatio="xMaxYMax"
-            >
-              <polygon
-                :ref="(el) => { introNodeRefs.line.value = el as SVGPolygonElement | null }"
-                class="fill-white"
-              />
-            </svg>
-            <div class="absolute inset-0 overflow-hidden">
-              <DAnimationAccordion
-                class="absolute bottom-8 -right-20 sm:right-5 -rotate-12 w-[clamp(300px,_33%,_500px)]"
-              />
-            </div>
-          </template>
-          <div id="home-intro-section">
-            <div class="relative h-full max-w-7xl mx-auto">
-              <DCharacter
-                :pose="currentPose"
-                shape-class="fill-black"
-                class="absolute self-end h-full -ml-20 sm:ml-auto mt-20 transition-all"
-              />
-              <DActionsFan
-                :actions="actions"
-                side="right"
-                class="absolute top-1/2 left-1/3 w-1/2 md:w-1/3 max-w-xs z-10 text-xl md:text-2xl"
-                filter-class="sharp-shadow ss-br-3 ss-neutral-50"
-                @action-focus="currentPose = $event"
-              />
-              <div class="absolute inset-0 text-3xl md:text-4xl">
-                <DWrapShape
-                  v-for="(socialLink, index) in socialLinks"
-                  :key="index"
-                  :ref="(el) => { introNodeRefs.socials.value[index] = el as ComponentPublicInstance }"
-                  class="absolute -top-1/3 left-1/2"
-                  shape-class="d-chip bg-black"
-                  filter-class="sharp-shadow ss-br-2 ss-white"
-                >
-                  <DBtn :href="socialLink.link" target="_blank">
-                    <Icon :name="socialLink.icon" class="m-[0.4em]" />
-                  </DBtn>
-                </DWrapShape>
-              </div>
-            </div>
-          </div>
-        </DWrapBackground>
-      </div>
-    </section>
+    <HomepageB1Intro />
     <!-- Block about sections -->
     <DWrapBackground
       id="sections"
@@ -375,39 +240,6 @@ const { data } = useAsyncData('homepage', async () => {
     </DWrapBackground>
   </div>
 </template>
-
-<!-- Intro -->
-<style>
-.intro-shape {
-  clip-path: polygon(3% 3%, 100% 0%, 95% 95%, 0% 100%);
-  animation: intro-shape-clip-path 5s linear infinite;
-  @apply backdrop-invert bg-neutral-900 bg-opacity-60;
-}
-
-@keyframes intro-shape-clip-path {
-  0%,
-  100% {
-    clip-path: polygon(3% 3%, 100% 0%, 95% 95%, 0% 100%);
-  }
-  25% {
-    clip-path: polygon(5% 3%, 96% 4%, 96% 97%, 0 99%);
-  }
-  50% {
-    clip-path: polygon(3% 2%, 100% 0, 90% 94%, 1% 91%);
-  }
-  75% {
-    clip-path: polygon(4% 7%, 96% 1%, 99% 99%, 8% 95%);
-  }
-}
-
-#home-intro-section {
-  width: 100%;
-  height: 75vh;
-  min-height: 35rem;
-  overflow: hidden;
-  @apply relative max-w-6xl mx-auto;
-}
-</style>
 
 <!-- Sections -->
 <style>
