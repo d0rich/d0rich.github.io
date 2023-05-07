0b320af77b69b79704f8f4caf1595299941089ef
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-07 12:30:28 +0400

    Update error 404 component

--- components/Error404.vue
+++ components/Error404.vue
@@ -4,12 +4,12 @@
     <DCharacter
       class="w-[clamp(300px,_50%,_430px)]"
       pose="idle"
-      outline
-      color
+      no-image
+      shape-class="fill-red-600"
     />
     <div>
-      <h1 class="text-4xl mb-4">Oops! Error 404</h1>
-      <p class="mb-4">Soory, but it seems this page does not exist :(</p>
+      <h1 class="text-4xl mb-4 text-red-400">Oops! Error 404</h1>
+      <p class="mb-4">Sorry, but it seems this page does not exist 😔</p>
       <DBigBangButton class="underline" text="To Homepage" to="/" />
     </div>
   </section>
