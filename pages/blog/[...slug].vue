aa9bf7ceed7aef684708629c534ea4fb275ae16b
Nikolay Dorofeev <dorich2000@gmail.com>
2023-05-07 16:56:32 +0400

    Add comments to blog posts

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Disqus } from 'vue-disqus'
+
 const slug = clearSlug(useRoute().params.slug as string[])
 
 const { data: doc } = useAsyncData(['blog', ...slug].join('/'), () =>
@@ -37,13 +39,14 @@ const linkToBlog = computed(() => {
 <template>
   <div v-if="doc" class="pb-[50vh] pt-10">
     <AsyncSafeSeoWithOg :title="doc.title" :description="doc.description" />
-    <div class="blog-article">
+    <div class="blog-article blog-fonts">
       <nav>
         <DBigBangButton text="< Back" :to="linkToBlog" />
       </nav>
       <time v-if="doc.date">{{ dateToDayMonthYear(doc.date) }}</time>
     </div>
-    <ContentRenderer :value="doc" tag="article" class="blog-article" />
+    <ContentRenderer :value="doc" tag="article" class="blog-article blog-fonts" />
+    <Disqus class="blog-fonts max-w-screen-md mx-auto px-3 mt-32" :identifier="doc._path" />
   </div>
   <Error404 v-else />
 </template>
@@ -55,8 +58,11 @@ const linkToBlog = computed(() => {
          dark:prose-headings:text-cyan-400
          dark:[&_p]:first-letter:bg-cyan-800
          dark:[&_:not(blockquote)_p]:first-letter:bg-transparent
-         dark:marker:text-cyan-400
-         [&_p]:!font-sans [&_a]:!font-sans [&_em]:!font-sans [&_li]:!font-sans
+         dark:marker:text-cyan-400;
+}
+
+.blog-fonts {
+  @apply [&_p]:!font-sans [&_a]:!font-sans [&_em]:!font-sans [&_li]:!font-sans
          [&_ul]:!font-sans [&_ol]:!font-sans [&_blockquote]:!font-sans
          [&_strong]:!font-sans [&_del]:!font-sans [&_hr]:!font-sans
          [&_table]:!font-sans [&_thead]:!font-sans [&_tbody]:!font-sans [&_tr]:!font-sans
