689decbe5ba160e8a33c91be1c0e4a108a51331a
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-11 15:30:17 +0400

    Blog surround docs links

---
+++ components/blog/SurroundDocCard.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+export default {
+  name: 'DLayoutSurroundDocCard'
+}
+</script>
+
+<script setup lang="ts">
+defineProps<{
+  doc: MarkdownParsedContent
+  direction: 'before' | 'after'
+}>()
+</script>
+
+<template>
+  <NuxtLink
+    :to="addTrailingSlash(doc._path)"
+    class="transition-transform hover:scale-90"
+  >
+    <DCard dense>
+      <DCardTitle v-if="direction === 'before'" class="[&_h3]:!text-cyan-800">
+        Older<Icon name="ic:sharp-arrow-forward-ios" />
+      </DCardTitle>
+      <DCardTitle v-else class="[&_h3]:!text-cyan-800">
+        <Icon name="ic:sharp-arrow-back-ios" />Newer
+      </DCardTitle>
+      <h4 class="text-xl font-bold">{{ doc.title }}</h4>
+      <p>{{ doc.description }}</p>
+    </DCard>
+  </NuxtLink>
+</template>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import { Disqus } from 'vue-disqus'
+import { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+
+interface Document extends MarkdownParsedContent {
+  date?: Date
+}
 
 const slug = clearSlug(useRoute().params.slug as string[])
-
-const { data: doc } = useAsyncData(['blog', ...slug].join('/'), () =>
-  queryContent('blog', ...slug).findOne()
-)
-
 const { itemsOnPage, filter } = useBlogNavigationConfig()
+const pagePath = ['/blog', ...slug].join('/')
+
+const { data: doc } = useAsyncData(pagePath, async () => {
+  const docPromise = queryContent<Document>('blog', ...slug).findOne()
+  const surroundPromise = queryContent<Document>()
+    .only(['_path', 'title', 'description'])
+    .where({
+      ...filter,
+      _path: /^\/blog/
+    })
+    .findSurround(pagePath, {
+      before: 1,
+      after: 1
+    })
+  const [doc, surround] = await Promise.all([docPromise, surroundPromise])
+  return {
+    ...doc,
+    before: surround[0] as Document,
+    after: surround[1] as Document
+  }
+})
 
 const { data: position } = useAsyncData(
   ['blog', ...slug, 'position'].join('/'),
@@ -16,7 +37,7 @@ const { data: position } = useAsyncData(
       .only('_path')
       .where({
         ...filter,
-        date: { $gte: doc.value?.date }
+        date: { $gte: Number(doc.value?.date) }
       })
       .find(),
   {
@@ -48,6 +69,20 @@ const linkToBlog = computed(() => {
       tag="article"
       class="blog-article blog-fonts"
     />
+    <nav
+      class="blog-article justify-center grid sm:grid-cols-2 gap-8 items-start my-16"
+    >
+      <BlogSurroundDocCard
+        v-if="doc.after"
+        :doc="doc.after"
+        direction="after"
+      />
+      <BlogSurroundDocCard
+        v-if="doc.before"
+        :doc="doc.before"
+        direction="before"
+      />
+    </nav>
     <nav class="blog-article blog-fonts my-10">
       <DBigBangButton text="< Back" :to="addTrailingSlash(linkToBlog)" />
     </nav>
