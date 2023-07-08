6beeb5db259cf62b21538f673911ce06fb7b2a32
Nikolay Dorofeev <dorich2000@gmail.com>
2023-07-08 10:49:24 +0400

    Move logic to client

--- composables/resume.ts
+++ composables/resume.ts
@@ -1,8 +1,84 @@
-import { ResumeData } from '~~/server/routes/api/resume/data'
+import type {
+  ParsedContent,
+  QueryBuilderWhere
+} from '@nuxt/content/dist/runtime/types'
+import type { TimeNote } from '~~/components/resume/TimeNote.vue'
+import type { D0xigenProjectMeta } from '~~/server/utils/types'
+
+interface TaggedParsedContent extends ParsedContent {
+  tags?: string[]
+  projects?: {
+    tags?: string[]
+  }
+}
+
+export type ResumeData = {
+  lead: TaggedParsedContent
+  contacts: ParsedContent
+  languages: ParsedContent
+  skills: TaggedParsedContent[]
+  work: TimeNote[]
+  projects: D0xigenProjectMeta[]
+  certificates: TimeNote[]
+  education: TimeNote[]
+}
 
 export function useResumeData() {
   const resumeType = computed(() => useRoute().params.type as string)
-  return useFetch<ResumeData>('/api/resume/data', {
-    query: { resumeType: resumeType.value }
+  return useAsyncData(() => {
+    return new Promise<ResumeData>((resolve, reject) => {
+      queryContent<TaggedParsedContent>('resume', 'leads', resumeType.value)
+        .findOne()
+        .then((lead) => {
+          const filterObject: QueryBuilderWhere = lead.tags
+            ? {
+                tags: { $containsAny: lead.tags }
+              }
+            : {}
+          Promise.all([
+            queryContent('/resume/contacts').findOne(),
+            queryContent('/resume/languages').findOne(),
+            queryContent<TaggedParsedContent>('/resume/skills')
+              .where(filterObject)
+              .find(),
+            queryContent<TimeNote>('/resume/work')
+              .sort({ 'daterange.end': -1 })
+              .find(),
+            $fetch('/api/projects/by-tags', {
+              query: { tags: (lead.projects?.tags ?? []).join(',') }
+            }),
+            queryContent<TimeNote>('/resume/certificates')
+              .sort({ 'daterange.end': -1 })
+              .find(),
+            queryContent<TimeNote>('/resume/education')
+              .sort({ 'daterange.end': -1 })
+              .find()
+          ])
+            .then(
+              ([
+                contacts,
+                languages,
+                skills,
+                work,
+                projects,
+                certificates,
+                education
+              ]) => {
+                resolve({
+                  lead,
+                  contacts,
+                  languages,
+                  skills,
+                  work,
+                  projects,
+                  certificates,
+                  education
+                })
+              }
+            )
+            .catch(reject)
+        })
+        .catch(reject)
+    })
   })
 }
